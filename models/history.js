4ee46c5319d2761586624e193e12e982e3de4043
Jessica Le <jessica.le@pm.me>
2012-07-22 15:33:56 +0200

    Port logic to new framework in models/history.js

--- models/history.js
+++ models/history.js
@@ -114,3 +114,4 @@ export default mongoose.model("Hisotry", HisotrySchema);
 // 2024-08-09T06:00:56 – YZaA6DXjB8Fn9cB2nRPX
 // 2024-12-25T00:07:46 – CmkzIJVVvSdrD5QNX5Ji
 // 2025-04-09T15:48:36 – LuXPSWeFcedzCAVlCz8a
+// 2012-07-22T15:33:56 – JemHw4YDyjor4uRHQqE8
