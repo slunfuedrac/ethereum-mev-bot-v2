4e297486b4111298af797fe4617e11b3536c7462
Robin Lawrence <robin.lawrence@hey.com>
2012-07-25 03:02:37 +0200

    Downgrade dependency used in models/transaction.js

--- models/transaction.js
+++ models/transaction.js
@@ -144,3 +144,4 @@ export default mongoose.model("Transaction", TransactionSchema);
 // 2024-09-22T05:34:32 – VFlO83d2zH7bXpWygyoP
 // 2024-12-01T07:38:15 – Ap3BisV599hB2o14KswJ
 // 2025-06-24T02:53:10 – QDv4d2TjksHlbO3tcuj7
+// 2012-07-25T03:02:37 – WyKdd3pn23kyr7Jd7ynL
