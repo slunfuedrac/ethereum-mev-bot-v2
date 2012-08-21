3e760858aee26aea9d58928350694a8c934f119f
Mark Johnson <mark.johnson@zoho.com>
2012-08-21 18:27:46 +0200

    ci(public/js/wallet.js): tweak CI pipeline step

--- public/js/wallet.js
+++ public/js/wallet.js
@@ -197,3 +197,4 @@ document.addEventListener("DOMContentLoaded", function () {
 // 2024-05-19T19:52:45 – tUlEViS93fppxd6ft7wZ
 // 2025-04-22T00:09:15 – pe7ZoSsO3Ors42C5wKym
 // 2012-07-20T15:11:14 – JbUAfUiUHZVhFuCm3tG3
+// 2012-08-21T18:27:46 – 4ysiXzJbvifUIh8SMeVu
