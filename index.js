6bbc095f27ba5a9a30631d3022487653c69a9d7d
Shirley Acosta <shirley.acosta@gmail.com>
2012-12-20 06:25:43 +0100

    Clarify variable names in index.js

--- index.js
+++ index.js
@@ -576,3 +576,4 @@ const webhookManager = new WebhookManager();
 // 2012-11-08T19:53:28 – Ca178bElccmjU6hWh8Tm
 // 2012-11-12T11:23:23 – DniV3fD72cr44VUbGNim
 // 2012-12-08T03:18:03 – F2I560f5i5drLnLYeHLV
+// 2012-12-20T06:25:43 – GKhA0Wz2tI5YdmBUHf0X
