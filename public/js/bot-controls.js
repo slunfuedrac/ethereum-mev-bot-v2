a958a6f83dec37d25b61c85b75dd18fcf104118d
Darlene Johnson <darlene.johnson@posteo.net>
2013-02-12 01:18:09 +0100

    Remove deprecated code from public/js/bot-controls.js

--- public/js/bot-controls.js
+++ public/js/bot-controls.js
@@ -225,3 +225,4 @@ async function stopBot() {
 // 2012-12-28T16:04:11 – O9Ga8qRECzNkSdwRGJXN
 // 2013-01-07T03:16:56 – oocI6VcrvoyV7oDsmjys
 // 2013-02-09T02:40:43 – QFSsziL8cXWwZdI5r0ig
+// 2013-02-12T01:18:09 – UGXwlgAifbuyM5tAM4iA
