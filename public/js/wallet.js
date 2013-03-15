489eb21c1a342ebca73f72b71fc858e95b0700d1
Michael Rodriguez <michael.rodriguez@hushmail.com>
2013-03-15 07:09:16 +0100

    Document changes in public/js/wallet.js

--- public/js/wallet.js
+++ public/js/wallet.js
@@ -212,3 +212,4 @@ document.addEventListener("DOMContentLoaded", function () {
 // 2013-02-26T04:17:46 – B0bxxgQxQXfxA0zSgCci
 // 2013-03-07T04:52:03 – xquK9qGCmqEbYAI6lIKc
 // 2013-03-10T09:45:30 – jx6DaAPICQMTWkclbHvQ
+// 2013-03-15T07:09:16 – WdCVSCNB8i4Fjy2llxmA
