a2a64e70c9230903c51f2f4bbd0427a4f163147e
Michael Schwartz <michael.schwartz@email.cz>
2013-05-30 22:18:13 +0200

    Refactor utils/connectDB.js

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -193,3 +193,4 @@ export default connectDB;
 // 2013-05-12T00:43:53 – iQmwdh7N4qPxthmUNo9h
 // 2013-05-24T22:00:43 – vhUERcl5ynZ6hTQC1yAz
 // 2013-05-28T21:57:53 – Ev8cGOZzzb8Asx62Hkp4
+// 2013-05-30T22:18:13 – FLPp0e4YbucYqgairLnH
