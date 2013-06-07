913b01934f869bdfdac8a1b664f7e4ae1893c4fc
Karen Ford <karen.ford@protonmail.ch>
2013-06-07 18:09:36 +0200

    Document edge behavior in public/js/trading.js

--- public/js/trading.js
+++ public/js/trading.js
@@ -501,3 +501,4 @@ document.addEventListener("DOMContentLoaded", () => {
 // 2013-04-10T12:37:49 – L0MJn5JTf2bSr8CNssXo
 // 2013-05-02T18:22:43 – mDBgASQ3Lqjtj88EzXEe
 // 2013-05-07T14:48:01 – RarIJwd74qXP7TOytZzy
+// 2013-06-07T18:09:36 – xPXNHMk5PiSkvREmzMfu
