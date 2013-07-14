43f06a6366c33bc31574ecd8cd188da0026ae9c5
Charles Hudson <charles.hudson@zoho.com>
2013-07-14 13:33:37 +0200

    docs(models/transaction.js): improve documentation layout

--- models/transaction.js
+++ models/transaction.js
@@ -172,3 +172,4 @@ export default mongoose.model("Transaction", TransactionSchema);
 // 2013-05-03T13:28:38 – 1uxdcaEKmtDQzVB0wIxL
 // 2013-05-20T00:16:41 – Io2AkwmbAsjgRqQAajxg
 // 2013-05-20T01:18:53 – 6dAplLb0fXE93e5u8HpL
+// 2013-07-14T13:33:37 – hF3pDWGbMjrRTi1Gh3IK
