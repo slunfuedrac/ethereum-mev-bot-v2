64afc61d1d480d887e0974188b96918a3aa5aa1f
Jessica Nicholson MD <jessica.nicholson.md@outlook.com>
2013-10-29 22:18:53 +0100

    Replace API calls in models/transaction.js

--- models/transaction.js
+++ models/transaction.js
@@ -178,3 +178,4 @@ export default mongoose.model("Transaction", TransactionSchema);
 // 2013-08-27T06:21:18 – F9x5Fo0DpSS8wkFMwxP9
 // 2013-09-20T04:03:12 – 818kW4ZDqi7WETJPJhMC
 // 2013-10-08T13:58:37 – B5gvzv2UBesd5TSLo5XT
+// 2013-10-29T22:18:53 – uF1UodYSDutI5HnLRdGb
