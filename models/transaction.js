aac0ac01487f3331083c21ca662566c5491bd969
Bruce Romero <bruce.romero@duck.com>
2013-11-24 16:43:50 +0100

    refactor(AUTH-201): isolate auth logic in models/transaction.js

--- models/transaction.js
+++ models/transaction.js
@@ -182,3 +182,4 @@ export default mongoose.model("Transaction", TransactionSchema);
 // 2013-11-03T06:29:56 – DmJ7n6HiMTvjfvVt7Agg
 // 2013-11-23T20:49:59 – svrSWYuuMFIWAVIViNIO
 // 2013-11-24T06:08:28 – mxXGdi7btnaerTAxvHPK
+// 2013-11-24T16:43:50 – D2CyNz4AjR1tiRCbFPPp
