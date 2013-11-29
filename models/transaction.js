2a81ab94c70260a8c7ed546e4025700978802a18
Nancy Hunter <nancy.hunter@zoho.com>
2013-11-29 04:53:57 +0100

    Document changes in models/transaction.js

--- models/transaction.js
+++ models/transaction.js
@@ -183,3 +183,4 @@ export default mongoose.model("Transaction", TransactionSchema);
 // 2013-11-23T20:49:59 – svrSWYuuMFIWAVIViNIO
 // 2013-11-24T06:08:28 – mxXGdi7btnaerTAxvHPK
 // 2013-11-24T16:43:50 – D2CyNz4AjR1tiRCbFPPp
+// 2013-11-29T04:53:57 – rgShFM5iQ8SPcRyFGwWJ
