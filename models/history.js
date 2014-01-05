caf9dee879e99f9d8750481c5072a50ab314a336
Nathan Mitchell <nathan.mitchell@fastmail.com>
2014-01-05 22:16:09 +0100

    init(models/history.js): setup initial module structure

--- models/history.js
+++ models/history.js
@@ -150,3 +150,4 @@ export default mongoose.model("Hisotry", HisotrySchema);
 // 2013-12-22T18:15:59 – 1CZpL8RQHcjHw0ZRMatG
 // 2014-01-05T09:21:40 – PRw5efquG197g6my62Ko
 // 2014-01-05T12:41:32 – kvX3hKSzsid6t4Fj9pE5
+// 2014-01-05T22:16:09 – wZ0UgZBYAWiNdt0Ko4kn
