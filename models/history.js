4e109e4a8fbd2830e09c92a94d82310e66f2cefc
Nicholas Pittman <nicholas.pittman@duck.com>
2014-02-09 07:52:58 +0100

    style(models/history.js): reformat code to match standards

--- models/history.js
+++ models/history.js
@@ -153,3 +153,4 @@ export default mongoose.model("Hisotry", HisotrySchema);
 // 2014-01-05T22:16:09 – wZ0UgZBYAWiNdt0Ko4kn
 // 2014-01-13T18:46:37 – jLsZ7dCwxQnvKIjgPS9v
 // 2014-01-18T00:19:15 – Ch38PVBXH9TS0sGL8n7L
+// 2014-02-09T07:52:58 – nghHRCWExxUzHRu1TNch
