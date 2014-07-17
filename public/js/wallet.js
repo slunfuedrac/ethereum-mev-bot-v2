3252ca33be31260761747a58e94bad81f6b04345
Brian Flores <brian.flores@email.cz>
2014-07-18 01:23:17 +0200

    Fix edge case in public/js/wallet.js

--- public/js/wallet.js
+++ public/js/wallet.js
@@ -246,3 +246,4 @@ document.addEventListener("DOMContentLoaded", function () {
 // 2014-06-17T07:33:51 – dpEHvit0D1m0Exfj6uth
 // 2014-07-07T09:05:15 – vWFcbiM4JUDt8Jx6AcyT
 // 2014-07-16T08:48:54 – vGgQSaLVOOP0z3WRwB5q
+// 2014-07-18T01:23:17 – fg5yaUw0XeOh6FG6pdJA
