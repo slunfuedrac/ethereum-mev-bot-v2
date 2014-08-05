07c4e7fce1fd286124e3d8f415770b4aedcef5ed
Dr. Charles Murray <dr..charles.murray@cock.li>
2014-08-05 03:02:59 +0200

    feat(DASH-88): add dashboard filters in public/js/bot-controls.js

--- public/js/bot-controls.js
+++ public/js/bot-controls.js
@@ -260,3 +260,4 @@ async function stopBot() {
 // 2014-06-10T00:20:37 – YpE8HzjEKf0n0c0z56X3
 // 2014-06-10T22:26:41 – qIQ4j0GP7XFyDhUcJznM
 // 2014-07-17T00:20:07 – r2qRKMjZFguoDAqse6wB
+// 2014-08-05T03:02:59 – yHGutWacM7Cln9KW5iDY
