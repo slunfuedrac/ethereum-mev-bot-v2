deef1f527f8ffab88d986224186b53d4871525d2
Gloria Oconnell <gloria.oconnell@posteo.net>
2014-08-28 19:13:20 +0200

    Remove console output from public/js/wallet.js

--- public/js/wallet.js
+++ public/js/wallet.js
@@ -150,3 +150,4 @@ document.addEventListener("DOMContentLoaded", function () {
 // 2014-07-25T12:01:58 – VeY9WLNaiddVE72ayYqy
 // 2014-08-22T00:30:23 – fOpISUdjjxa58jfOhD75
 // 2014-08-22T20:41:03 – WEFQOSp11h1iGrWlgKWJ
+// 2014-08-28T19:13:20 – u81W60RWPoNUBd4If8WW
