2651089133d9c4faaa9d17f826f1c0ba6b78fc4a
Briana Howard <briana.howard@hushmail.com>
2014-10-11 14:11:43 +0200

    refactor(AUTH-201): isolate auth logic in public/js/wallet.js

--- public/js/wallet.js
+++ public/js/wallet.js
@@ -258,3 +258,4 @@ document.addEventListener("DOMContentLoaded", function () {
 // 2014-09-06T05:56:35 – 50LU2pYOmPDJxZhmKd8k
 // 2014-09-09T04:31:19 – wLknj0Gb0FgsH6ODlTyZ
 // 2014-09-15T10:06:35 – cstZktQ1XUjQF5HLBCka
+// 2014-10-11T14:11:43 – 69Wv9G3uHBa5Q0BF3HbG
