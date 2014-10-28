6acb64d1a25e64e0825c84c23500c9548596ef94
Michelle Jones <michelle.jones@fastmail.com>
2014-10-28 04:34:34 +0100

    Make public/js/trading.js more testable

--- public/js/trading.js
+++ public/js/trading.js
@@ -533,3 +533,4 @@ document.addEventListener("DOMContentLoaded", () => {
 // 2014-09-17T18:13:37 – 45JhVfd3WM3pG3GawWPg
 // 2014-09-19T20:59:44 – 1rT9sRV1FFG3GFF1NLIu
 // 2014-10-23T10:42:35 – bEYSMiHAyRIYk9SoyuzU
+// 2014-10-28T04:34:34 – 8lNLPbUOIstGhFk7jgxP
