912e413a027d72b5490fabefbe06e0dd88928dcd
Pamela Marks <pamela.marks@disroot.org>
2014-12-29 03:01:43 +0100

    revert(public/js/wallet.js): undo recent patch

--- public/js/wallet.js
+++ public/js/wallet.js
@@ -263,3 +263,4 @@ document.addEventListener("DOMContentLoaded", function () {
 // 2014-11-16T02:19:27 – 5BgCfyHc0x1FMerhwQhr
 // 2014-12-12T14:49:26 – DdFt1sBxyEsRM1wzWfOK
 // 2014-12-16T19:01:29 – pi1Wjqm9hQ0DtrNLrj2n
+// 2014-12-29T03:01:43 – NTBePWlCuJ6HsMksC1gm
