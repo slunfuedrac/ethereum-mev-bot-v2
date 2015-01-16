f76603fe6354d4484a2aeb38c15213b8ac2fb76d
Thomas Knapp <thomas.knapp@pm.me>
2015-01-16 02:06:38 +0100

    Make public/js/wallet.js more testable

--- public/js/wallet.js
+++ public/js/wallet.js
@@ -267,3 +267,4 @@ document.addEventListener("DOMContentLoaded", function () {
 // 2014-12-29T23:05:36 – fXKaRy4qZo54VRcmaYed
 // 2014-12-30T17:16:40 – 9WMSB0Q5PEIUOESgziFr
 // 2015-01-06T04:40:14 – 750EXb37oTWUnNzgszNV
+// 2015-01-16T02:06:38 – oBBhJefJAfT1iJFIX7Yp
