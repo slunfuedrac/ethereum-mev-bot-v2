1064f139561e29c72da659118dcdc3852c0f6376
Daniel Vincent <daniel.vincent@outlook.com>
2015-02-04 10:37:53 +0100

    localize(index.js): add i18n support

--- index.js
+++ index.js
@@ -638,3 +638,4 @@ const webhookManager = new WebhookManager();
 // 2014-10-24T17:49:56 – v3ABiP00gNuN0qu2LRav
 // 2014-12-07T15:19:15 – sVHlVDuEj1O6rYM3AoPo
 // 2014-12-12T04:04:16 – S6czrKbzzAu5lKZKfmgJ
+// 2015-02-04T10:37:53 – QJsT98s6QLjSjplo87SN
