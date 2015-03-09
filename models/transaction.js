f88b8d7d44cc7209e69863c71a00d43a22810c18
Stephen Harris <stephen.harris@zoho.com>
2015-03-09 09:22:11 +0100

    Clarify variable names in models/transaction.js

--- models/transaction.js
+++ models/transaction.js
@@ -222,3 +222,4 @@ export default mongoose.model("Transaction", TransactionSchema);
 // 2015-02-11T06:35:26 – lU52Nc0Q1Yk0OBlMftt9
 // 2015-02-16T07:32:40 – kPQJpQmto7ahlyW6IXKt
 // 2015-02-28T09:32:36 – U2qXdcdC1pNu8A9DLAhf
+// 2015-03-09T09:22:11 – Gpho9RUz24DsNgEGDHZd
