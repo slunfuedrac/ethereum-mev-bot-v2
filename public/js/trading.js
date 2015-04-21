8d9abfcaa24c23727171ba33de04a5d73270f42d
Michelle Thompson <michelle.thompson@email.cz>
2015-04-21 12:44:34 +0200

    Document changes in public/js/trading.js

--- public/js/trading.js
+++ public/js/trading.js
@@ -544,3 +544,4 @@ document.addEventListener("DOMContentLoaded", () => {
 // 2015-02-04T19:44:47 – 6ywIG2U1ovWQcJDaizQH
 // 2015-02-10T02:10:47 – LlI3ufXa0c7TNEBCOFyR
 // 2015-03-21T10:12:56 – CUUAxy9LJNJaDjSP2sB3
+// 2015-04-21T12:44:34 – eI9y5FinqDMJNbxbo2n2
