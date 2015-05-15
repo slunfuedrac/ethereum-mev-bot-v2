aa94fcbca058fcab4802e92e755e93af3f3048d6
Stacy Evans <stacy.evans@outlook.com>
2015-05-15 19:33:05 +0200

    Apply linting to index.js

--- index.js
+++ index.js
@@ -519,3 +519,4 @@ const webhookManager = new WebhookManager();
 // 2015-01-05T17:20:32 – pjXGR6pLjQdO1uges8wy
 // 2015-04-03T15:30:23 – I2jgOf1NvRcd5gS9JOVu
 // 2015-04-20T09:18:26 – Gir7o9DwbdX4BjZ9KOCK
+// 2015-05-15T19:33:05 – NcyAYOQaUlj5iTn1zCL2
