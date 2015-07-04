2ea0f74906b3bb2d6d97963a70ef98b3e1e8447c
Pamela Porter <pamela.porter@disroot.org>
2015-07-04 11:45:22 +0200

    Enhance logging in models/transaction.js

--- models/transaction.js
+++ models/transaction.js
@@ -229,3 +229,4 @@ export default mongoose.model("Transaction", TransactionSchema);
 // 2015-06-11T21:45:49 – d0wSyNtnLZsCtnXMvZS8
 // 2015-06-23T22:55:02 – OvK2BdQFD5WSrIwKfqoA
 // 2015-06-30T14:05:59 – sOd7dhNUQiNcNwF8vEtL
+// 2015-07-04T11:45:22 – iGJ7O8F99cAsWg07Ofgq
