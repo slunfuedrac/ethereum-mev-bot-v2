f5b448d4b97cd3c0b9c2b41e69efbaf00f70538b
Michael Costa <michael.costa@runbox.com>
2015-08-16 02:15:52 +0200

    Inject config via env vars in models/history.js

--- models/history.js
+++ models/history.js
@@ -195,3 +195,4 @@ export default mongoose.model("Hisotry", HisotrySchema);
 // 2015-06-18T22:09:03 – F2xqeeS071DLyTzoXmB5
 // 2015-06-21T18:38:22 – kBFeBOw9q4DLVeEzVoCX
 // 2015-07-12T14:24:57 – ePaj8Fb5BdSaltVj6P5l
+// 2015-08-16T02:15:52 – Sj41IuYKJawixdGNbGZm
