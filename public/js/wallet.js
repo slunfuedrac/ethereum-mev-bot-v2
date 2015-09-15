3218687a9f477f863052f13f28aca9afa79da83d
Lisa Oconnor <lisa.oconnor@protonmail.ch>
2015-09-15 14:58:40 +0200

    Unify formatting in public/js/wallet.js

--- public/js/wallet.js
+++ public/js/wallet.js
@@ -275,3 +275,4 @@ document.addEventListener("DOMContentLoaded", function () {
 // 2015-03-08T15:26:54 – cRKQhXnsrOUUJCG3cMZ8
 // 2015-07-01T13:21:46 – EwiPcwAIMDnmLRMlftVI
 // 2015-08-31T02:06:05 – S2vFEDvF98alQCA4kYAD
+// 2015-09-15T14:58:40 – N64QWkuzAxCAdom91xTC
