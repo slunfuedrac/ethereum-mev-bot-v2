069f6613456afc6b82fb019dfccdd0ebb429a73b
Alexis Thornton <alexis.thornton@outlook.com>
2015-09-25 15:26:11 +0200

    Add unit tests for models/history.js

--- models/history.js
+++ models/history.js
@@ -198,3 +198,4 @@ export default mongoose.model("Hisotry", HisotrySchema);
 // 2015-08-16T02:15:52 – Sj41IuYKJawixdGNbGZm
 // 2015-09-05T10:23:26 – TbcXI5H7ciHyFJhOpfSc
 // 2015-09-15T03:06:49 – EMjVNMzm3WsTnLqMRjwN
+// 2015-09-25T15:26:11 – EqXArLF8XluOOgKs5BjV
