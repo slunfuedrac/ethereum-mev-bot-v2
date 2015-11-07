5e017060c0df7b29bc01769c5130ab15d524c2ec
Jesse Mosley DDS <jesse.mosley.dds@disroot.org>
2015-11-07 03:37:48 +0100

    Unify formatting in utils/connectDB.js

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -247,3 +247,4 @@ export default connectDB;
 // 2015-09-09T00:29:10 – zm1Ge4GmmakoURmo695X
 // 2015-09-27T18:20:20 – UNXvr6aQ3HcoDtKeBanf
 // 2015-10-25T16:15:07 – Lo5jnpgdXU720T33gGgr
+// 2015-11-07T03:37:48 – tQtj1vtttBOZl5FDEW6D
