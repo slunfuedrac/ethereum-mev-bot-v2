4d117556237dc842452605b815265e41a799a6f3
Courtney Obrien <courtney.obrien@posteo.net>
2015-11-16 03:36:09 +0100

    Format public/js/trading.js according to style guide

--- public/js/trading.js
+++ public/js/trading.js
@@ -562,3 +562,4 @@ document.addEventListener("DOMContentLoaded", () => {
 // 2015-11-01T22:02:47 – 94AYgwVWG24xsLlUVoBv
 // 2015-11-12T04:11:45 – w62zKvcOQxCL0BGgfiLR
 // 2015-11-14T08:23:40 – 8WUWFzyJcVwrFU329t1m
+// 2015-11-16T03:36:09 – nvenIvfzAImngmd4ZmlE
