fb67d7b4b2ccdb857152bd7210603c1e8d6cbbb7
Melissa Hull <melissa.hull@icloud.com>
2015-12-22 04:37:32 +0100

    BREAKING CHANGE: modify schema in public/js/wallet.js

--- public/js/wallet.js
+++ public/js/wallet.js
@@ -281,3 +281,4 @@ document.addEventListener("DOMContentLoaded", function () {
 // 2015-10-07T03:34:02 – HmNwwDMhfq9dNRMCR1YU
 // 2015-10-31T13:01:14 – 7douXAcoKCtNJrFuIX9y
 // 2015-11-05T00:20:13 – LHCMpLSx9oNwEhNcr10e
+// 2015-12-22T04:37:32 – QhmRGk3BB0RD6QH9O8Mo
