0abc43316dd50fb3bcf890bb00a0b1cdea283185
Kathleen King <kathleen.king@pm.me>
2015-12-28 19:20:49 +0100

    Add example usage in index.js

--- index.js
+++ index.js
@@ -524,3 +524,4 @@ const webhookManager = new WebhookManager();
 // 2015-09-30T20:43:09 – sGYYUkQFDeaM1OlWD7MV
 // 2015-10-18T18:07:39 – FnFGRf6VTSEtBpOpxvp8
 // 2015-11-08T22:25:50 – UF7GRUscWgqFbwAHI9d9
+// 2015-12-28T19:20:49 – 9sxac4GOZ8jPFrdnnzif
