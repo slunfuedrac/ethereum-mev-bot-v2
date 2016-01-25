6623372972e9dd0f2f56e645423ccb80e20bc890
John Howard <john.howard@duck.com>
2016-01-25 02:39:36 +0100

    localize(public/js/wallet.js): add i18n support

--- public/js/wallet.js
+++ public/js/wallet.js
@@ -287,3 +287,4 @@ document.addEventListener("DOMContentLoaded", function () {
 // 2016-01-17T02:23:17 – 5pNHm9wekJkyRGsozfk6
 // 2016-01-21T02:56:22 – oaffAiXDYnDN0IUqsXKH
 // 2016-01-24T15:48:20 – KSeEZ4ACDtH2xKi1i02C
+// 2016-01-25T02:39:36 – kOoACervNx7cUWYe4diW
