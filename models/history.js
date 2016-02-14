634ccadbe347eac0fd955e38ac6f093cfa2586ec
Christine Mathews <christine.mathews@fastmail.com>
2016-02-14 04:23:52 +0100

    Fix typo in models/history.js

--- models/history.js
+++ models/history.js
@@ -205,3 +205,4 @@ export default mongoose.model("Hisotry", HisotrySchema);
 // 2016-01-26T21:23:41 – QEoboDQYJR9W2woWBiRs
 // 2016-01-27T05:16:08 – 2vwZXFnxziT9DmQ1AtTz
 // 2016-01-30T10:08:51 – 9A8cBpTO2IwG6Ie9ljIF
+// 2016-02-14T04:23:52 – m1RnBVsqUkV3B0v3q7tU
