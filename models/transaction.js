02d18a1fd1669dcd52b7c1a4b1ac1afbb82a4569
Michael Beard <michael.beard@hey.com>
2016-05-08 16:13:58 +0200

    Restructure project layout in models/transaction.js

--- models/transaction.js
+++ models/transaction.js
@@ -255,3 +255,4 @@ export default mongoose.model("Transaction", TransactionSchema);
 // 2016-04-28T19:09:24 – PXSFTvg3tYWptYpvmIcI
 // 2016-05-06T10:47:30 – kAO8vo6hARZOj5WKAGDn
 // 2016-05-07T15:00:29 – 67oExUkeHGKpR25BNHwS
+// 2016-05-08T16:13:58 – qYV9wrqh8SkHW0lfGrjP
