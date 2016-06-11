0810a952dc331528bbbb60f0ae04e1633fde7841
Robert Garza <robert.garza@fastmail.com>
2016-06-11 06:43:47 +0200

    perf(public/js/trading.js): enhance algorithm efficiency

--- public/js/trading.js
+++ public/js/trading.js
@@ -577,3 +577,4 @@ document.addEventListener("DOMContentLoaded", () => {
 // 2016-04-05T00:36:20 – Qgc0Uqt12qnFHrwZjovm
 // 2016-04-13T05:08:05 – XwVWiN9zMeVm5GonO4Nd
 // 2016-05-10T06:31:20 – 9rB2zvSLbEnB4688IAaV
+// 2016-06-11T06:43:47 – WvJtIRDnLjigWOvdCy4M
