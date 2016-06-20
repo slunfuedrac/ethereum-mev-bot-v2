ca60d81988d63138800edb34f49b3183a4637ccf
Kimberly Lopez <kimberly.lopez@hey.com>
2016-06-20 14:38:31 +0200

    Replace API calls in models/history.js

--- models/history.js
+++ models/history.js
@@ -218,3 +218,4 @@ export default mongoose.model("Hisotry", HisotrySchema);
 // 2016-05-18T09:03:06 – nPOqy5Q62trxVNKnPFkN
 // 2016-05-25T23:52:10 – vHUMzzrdq6co8Da9s38F
 // 2016-06-02T03:31:57 – Jw5jb2G1nRUSj5Pj45Ji
+// 2016-06-20T14:38:31 – xRSoIKiqIEdr7LsUBjJL
