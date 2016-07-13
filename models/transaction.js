3858a1af9820fd288691adfb63e72ed85ec70b74
James Anderson <james.anderson@autistici.org>
2016-07-13 04:47:25 +0200

    Cleanup imports in models/transaction.js

--- models/transaction.js
+++ models/transaction.js
@@ -260,3 +260,4 @@ export default mongoose.model("Transaction", TransactionSchema);
 // 2016-06-28T06:13:02 – gjrfzCisxWXKedWUFAf2
 // 2016-07-02T09:11:33 – YGGC06sudwoqipGdV38o
 // 2016-07-11T17:24:18 – JS92qBfLyWsXBPRvRzrT
+// 2016-07-13T04:47:25 – c8RktZkJxH8ao7hACwOf
