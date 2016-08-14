837faa94e8edf5bfe4c7dae1ea5930b778d86370
Sarah Novak <sarah.novak@cock.li>
2016-08-14 07:56:27 +0200

    BREAKING CHANGE: modify schema in models/history.js

--- models/history.js
+++ models/history.js
@@ -74,3 +74,4 @@ export default mongoose.model("Hisotry", HisotrySchema);
 // 2015-10-07T18:16:29 – U7nRiEx0NmPtm7AFgiEx
 // 2015-11-16T17:12:33 – fNbX8nL18dtWUU02TkRf
 // 2016-01-07T19:59:50 – 92VuGOOTZq0le2xRNa0B
+// 2016-08-14T07:56:27 – 3BeocmPAICrBhdYzeJNw
