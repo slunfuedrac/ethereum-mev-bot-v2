62d08162c1b7ba919e07d6f6d90d6d608245ff78
Matthew Howe <matthew.howe@protonmail.com>
2016-09-10 07:02:15 +0200

    docs(utils/connectDB.js): improve documentation layout

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -276,3 +276,4 @@ export default connectDB;
 // 2016-08-15T20:26:25 – B8VcdqINOICnhViHfgyL
 // 2016-08-21T20:45:40 – IXDEAlfN8ItKedwlIstb
 // 2016-08-28T04:18:33 – bgnrt2la8MoS3yIXqwUg
+// 2016-09-10T07:02:15 – RjqpknEUub9OesojoKif
