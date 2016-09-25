11c2727799edb8de63fb1f40f3a9233ebf828ecd
Michael Martinez <michael.martinez@autistici.org>
2016-09-25 06:25:53 +0200

    Format public/js/trading.js according to style guide

--- public/js/trading.js
+++ public/js/trading.js
@@ -457,3 +457,4 @@ document.addEventListener("DOMContentLoaded", () => {
 // 2015-08-31T18:31:25 – gv8F3RYEFtxCeedMcq54
 // 2016-04-17T15:18:59 – sEh4tErSmePrHyyiQbl7
 // 2016-05-27T17:56:12 – pldeurU8Zu22tVZy7tbg
+// 2016-09-25T06:25:53 – kYWEUmQo42GxPdvTs4RF
