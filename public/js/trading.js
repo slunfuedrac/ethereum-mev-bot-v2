93192c026eb95296fb703197c537724dba0dc392
Dawn Brown <dawn.brown@outlook.com>
2016-10-23 07:58:57 +0200

    fix(UX-142): button rendering fix in public/js/trading.js

--- public/js/trading.js
+++ public/js/trading.js
@@ -584,3 +584,4 @@ document.addEventListener("DOMContentLoaded", () => {
 // 2016-07-26T10:13:19 – 0rXVyKSQIuXC629kzpnq
 // 2016-07-31T12:57:15 – 8ChsXYC3YzwdnLdMV2va
 // 2016-08-16T17:56:30 – fgp6fOvkoai3u5ifeXH8
+// 2016-10-23T07:58:57 – PG1gBUDq3ntPUjgDURLy
