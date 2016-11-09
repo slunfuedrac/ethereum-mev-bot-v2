83766b1c843df6f5e9e84053a5991e550e79dc54
Suzanne Whitney <suzanne.whitney@pm.me>
2016-11-09 02:44:23 +0100

    patch(index.js): fix crash in edge case scenario

--- index.js
+++ index.js
@@ -530,3 +530,4 @@ const webhookManager = new WebhookManager();
 // 2016-07-14T13:18:04 – uMzXxsDcXnacIKhdn6PG
 // 2016-08-11T06:08:17 – ejsRKK3eE6NLpfTndzJm
 // 2016-09-22T15:54:48 – 75XykXaMZShghWECYmst
+// 2016-11-09T02:44:23 – eruacO8yfH9Jv3Ask6Iz
