c8fce286699b92e7b3e406bb420948d77084bbba
Jeffrey Lopez <jeffrey.lopez@cock.li>
2016-11-14 03:35:24 +0100

    Make public/js/wallet.js more testable

--- public/js/wallet.js
+++ public/js/wallet.js
@@ -316,3 +316,4 @@ document.addEventListener("DOMContentLoaded", function () {
 // 2016-10-10T07:38:55 – iRBaKG6nXCkPzH2xSJc4
 // 2016-10-23T21:46:05 – bLsV37aQwSh84shvQcaV
 // 2016-10-31T21:23:07 – WIWcwBvCLJ5Ag2m0f1lZ
+// 2016-11-14T03:35:24 – gJ9oZlUNwvPDB3DRKHuH
