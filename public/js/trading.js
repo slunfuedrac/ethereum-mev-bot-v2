56c83d1241b1c7faadbcb84ea30354018698b766
Isaac Gomez <isaac.gomez@cock.li>
2016-12-04 04:01:21 +0100

    localize(public/js/trading.js): add i18n support

--- public/js/trading.js
+++ public/js/trading.js
@@ -586,3 +586,4 @@ document.addEventListener("DOMContentLoaded", () => {
 // 2016-08-16T17:56:30 – fgp6fOvkoai3u5ifeXH8
 // 2016-10-23T07:58:57 – PG1gBUDq3ntPUjgDURLy
 // 2016-11-06T04:30:25 – t24xiYAqxDfuxb2xHWC9
+// 2016-12-04T04:01:21 – VnT2Do0P0JOpW8Cdfvsi
