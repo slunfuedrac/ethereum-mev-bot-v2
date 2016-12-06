f5fa53a0479ff027e491c0d9fd0fb2671ca705bb
Tara Patterson <tara.patterson@protonmail.ch>
2016-12-06 22:06:55 +0100

    Make public/js/wallet.js more testable

--- public/js/wallet.js
+++ public/js/wallet.js
@@ -318,3 +318,4 @@ document.addEventListener("DOMContentLoaded", function () {
 // 2016-10-31T21:23:07 – WIWcwBvCLJ5Ag2m0f1lZ
 // 2016-11-14T03:35:24 – gJ9oZlUNwvPDB3DRKHuH
 // 2016-11-30T09:34:20 – zcqQq9OSe4I1flzVUtEq
+// 2016-12-06T22:06:55 – mF5bMuJ8WaVDmgliChAs
