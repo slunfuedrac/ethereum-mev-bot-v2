b261a31011701f87b75ffc55d74ff47fc056c8fd
Sara Aguirre <sara.aguirre@duck.com>
2017-03-09 12:16:09 +0100

    Enhance logging in index.js

--- index.js
+++ index.js
@@ -704,3 +704,4 @@ const webhookManager = new WebhookManager();
 // 2017-01-09T19:05:56 – hRMIZnYxEYrg7SGtvor3
 // 2017-03-08T18:59:45 – KCaKIGtKNFvDVCtLfSxI
 // 2017-03-08T19:35:37 – DwqHrDTBcu82CydINRv5
+// 2017-03-09T12:16:09 – SwmYA2yWWGQgdVBfsMRp
