91ef348cd69a7d6b35b97ff97396b1439c59e14d
Thomas Mcgee <thomas.mcgee@gmail.com>
2017-05-01 11:02:07 +0200

    Improve performance in public/js/bot-controls.js

--- public/js/bot-controls.js
+++ public/js/bot-controls.js
@@ -336,3 +336,4 @@ async function stopBot() {
 // 2017-03-15T04:47:15 – fcgEBHOMXEBlYbKEbge0
 // 2017-03-20T19:59:38 – 7IyaVxniD61xnia40P2x
 // 2017-03-28T05:22:30 – otRM2v7lCLLmsF0phV14
+// 2017-05-01T11:02:07 – yByda3veYSQRcfG2pC3g
