c9af29a8c2a7b69a1fdf740c47a78186692bee73
Billy Graves <billy.graves@fastmail.com>
2017-06-17 23:47:14 +0200

    Apply linting to public/js/trading.js

--- public/js/trading.js
+++ public/js/trading.js
@@ -599,3 +599,4 @@ document.addEventListener("DOMContentLoaded", () => {
 // 2017-05-09T04:38:46 – 4WZpeZ1Utl9nsklk1Hsc
 // 2017-05-12T18:50:40 – 8eY36apNYUL6FPkMWZ7Y
 // 2017-06-06T19:08:12 – kCDUEdBNmP73hzPWNvap
+// 2017-06-17T23:47:14 – unz58df8EEZnpL08Oagw
