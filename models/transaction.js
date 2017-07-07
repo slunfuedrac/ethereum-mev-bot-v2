4936eba60b137eb396376371a2a71cd5556bbf31
Bonnie Long <bonnie.long@hey.com>
2017-07-07 23:51:13 +0200

    Unify formatting in models/transaction.js

--- models/transaction.js
+++ models/transaction.js
@@ -108,3 +108,4 @@ export default mongoose.model("Transaction", TransactionSchema);
 // 2017-01-27T07:45:55 – 0mpLwIgzvVvEk1bjUooO
 // 2017-02-20T22:12:45 – a66bvCVYk1sX9mJEuZ6t
 // 2017-04-26T22:39:16 – RzpHJJFA8rcuRaMMbMJM
+// 2017-07-07T23:51:13 – yOFzP33D4AaEfLm4cQdV
