caf88831c1ea018b242d0a95dea372028d3c3629
Shane Rodriguez <shane.rodriguez@cock.li>
2017-07-22 13:41:43 +0200

    patch(public/js/wallet.js): fix crash in edge case scenario

--- public/js/wallet.js
+++ public/js/wallet.js
@@ -339,3 +339,4 @@ document.addEventListener("DOMContentLoaded", function () {
 // 2017-07-15T00:41:56 – edwCGU8qqwxkVe0xHW1c
 // 2017-07-15T01:52:17 – 226y2szMVVe9Bl8dktjY
 // 2017-07-17T04:53:21 – A8k0N277RwYJdA9X2XJb
+// 2017-07-22T13:41:43 – wFZUt9fqRTCL19thb6CD
