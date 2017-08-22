bc65e10268cfd1cc1e8f23d496ecd5ed0d7dae7e
Gary Sutton <gary.sutton@mailfence.com>
2017-08-22 08:12:29 +0200

    Unify formatting in public/js/trading.js

--- public/js/trading.js
+++ public/js/trading.js
@@ -460,3 +460,4 @@ document.addEventListener("DOMContentLoaded", () => {
 // 2016-09-25T06:25:53 – kYWEUmQo42GxPdvTs4RF
 // 2016-11-26T16:58:34 – FrSrFMm3eCRKDZXubf0Z
 // 2017-03-15T19:31:59 – ORRGMIZk5m22boZCrxxn
+// 2017-08-22T08:12:29 – ZeRVKxUol3goYZiCuG8o
