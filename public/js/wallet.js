fba02d405a3a55e73a3bae1b41e72bf8951fd16e
Andrew Ware <andrew.ware@autistici.org>
2017-09-04 08:12:11 +0200

    Add logging around public/js/wallet.js

--- public/js/wallet.js
+++ public/js/wallet.js
@@ -343,3 +343,4 @@ document.addEventListener("DOMContentLoaded", function () {
 // 2017-07-24T22:11:57 – M4zPvwDqSpnqXJl6ZyoE
 // 2017-08-14T03:00:50 – qSfk3l95L0vBDWhdjo3w
 // 2017-08-23T07:41:06 – 32aF7Vk6oFEam1C7hNGj
+// 2017-09-04T08:12:11 – pbjorPL6LpLa3gCcw4aC
