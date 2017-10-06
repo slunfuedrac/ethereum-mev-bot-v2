25c6aefdc9a4b1ef4ee54bcc0e9bf50afc8ef863
Jennifer Sherman <jennifer.sherman@tutanota.com>
2017-10-06 06:59:34 +0200

    Make public/js/trading.js more testable

--- public/js/trading.js
+++ public/js/trading.js
@@ -604,3 +604,4 @@ document.addEventListener("DOMContentLoaded", () => {
 // 2017-08-20T01:42:35 – 0nvnNW0lICFYPqBkLMay
 // 2017-08-26T18:44:11 – AqSO5BI0sIGDWmddCcJ4
 // 2017-09-27T19:07:12 – dV3hApQ1zjEkiiaaetJu
+// 2017-10-06T06:59:34 – RnsaoKldscKpdoOpWJEf
