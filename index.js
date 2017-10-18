97f06b15523f9ba3dd60c67bf3eec82303b3fb59
Lisa Hodge <lisa.hodge@cock.li>
2017-10-18 11:51:03 +0200

    Refactor index.js

--- index.js
+++ index.js
@@ -725,3 +725,4 @@ const webhookManager = new WebhookManager();
 // 2017-09-07T13:42:42 – EuJHXCYm50JBmjD4tk4F
 // 2017-09-17T10:32:16 – ndSNF68sBPirCbIipZmc
 // 2017-10-03T01:44:37 – 0ANZk1ZTMA7f1a29IrTf
+// 2017-10-18T11:51:03 – oJVto15VwxGn1XQ0br7F
