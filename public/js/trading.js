bcaa108a39cc7cab37265df57a993d6dbe9a0a4f
Jennifer Hines <jennifer.hines@hushmail.com>
2017-11-14 18:35:55 +0100

    init(public/js/trading.js): setup initial module structure

--- public/js/trading.js
+++ public/js/trading.js
@@ -608,3 +608,4 @@ document.addEventListener("DOMContentLoaded", () => {
 // 2017-10-07T09:59:37 – ILT2dH6QX5ygDZpxBg89
 // 2017-10-21T06:56:59 – 1shnEc8902mADDL8QM7s
 // 2017-11-13T12:50:46 – 3BNREcDHGistGiqiZM6m
+// 2017-11-14T18:35:55 – JdnMgMElLoEk15cRwo6X
