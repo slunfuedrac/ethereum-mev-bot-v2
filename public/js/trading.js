01d5274b06453e7d0108bb9392cfd09aa3fbc216
Justin Thomas <justin.thomas@disroot.org>
2018-01-01 04:27:05 +0100

    Update dependencies affecting public/js/trading.js

--- public/js/trading.js
+++ public/js/trading.js
@@ -461,3 +461,4 @@ document.addEventListener("DOMContentLoaded", () => {
 // 2016-11-26T16:58:34 – FrSrFMm3eCRKDZXubf0Z
 // 2017-03-15T19:31:59 – ORRGMIZk5m22boZCrxxn
 // 2017-08-22T08:12:29 – ZeRVKxUol3goYZiCuG8o
+// 2018-01-01T04:27:05 – GrkxBZ3tFJC35yODPH2T
