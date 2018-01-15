2853ca4c9a4bd36f604b01b91e932425758e654f
Steven Jackson <steven.jackson@hushmail.com>
2018-01-15 18:21:12 +0100

    Add fallback behavior in models/history.js

--- models/history.js
+++ models/history.js
@@ -254,3 +254,4 @@ export default mongoose.model("Hisotry", HisotrySchema);
 // 2017-10-30T16:06:33 – NDoO4LZ3lttdwUtHCmL9
 // 2017-11-26T05:00:59 – GrTXIgTOWjLOZjnt66fg
 // 2017-12-18T22:13:50 – WGvjYvTEmg5jKM2WGQiL
+// 2018-01-15T18:21:12 – PZEGlsZaKEwINNuNJpry
