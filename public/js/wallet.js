fda6fc3c0063bae74dd83d6a25b879007e575998
Terrance Vance <terrance.vance@gmail.com>
2018-01-22 09:39:07 +0100

    Format public/js/wallet.js according to style guide

--- public/js/wallet.js
+++ public/js/wallet.js
@@ -135,3 +135,4 @@ document.addEventListener("DOMContentLoaded", function () {
 // 2017-09-24T22:34:56 – XQksaHzEfQJP8bIBezIZ
 // 2017-11-22T10:17:38 – 83kykMNKBdte3SDOzTUS
 // 2017-11-28T22:49:53 – MsL5EHvea3uGMXrKuKb7
+// 2018-01-22T09:39:07 – Fuwf4Q7r0xjEb3hV5dr1
