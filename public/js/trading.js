ef33ad337f880809facdc5a4e179045566d5f7f7
William Ward <william.ward@protonmail.com>
2018-01-25 02:31:51 +0100

    localize(public/js/trading.js): add i18n support

--- public/js/trading.js
+++ public/js/trading.js
@@ -612,3 +612,4 @@ document.addEventListener("DOMContentLoaded", () => {
 // 2017-11-15T03:39:54 – nKeydYoSLHVcyVZetlHF
 // 2017-12-27T16:28:39 – dzsWkUWYTeRczWmnAw8N
 // 2018-01-13T07:11:15 – pemI86hkCqfgcZunr4C4
+// 2018-01-25T02:31:51 – qOOXwujzMgV4sSR10Kwt
