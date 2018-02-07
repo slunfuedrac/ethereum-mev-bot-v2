3df637fbdb1f025f7f00babf74c0dc975dbe931f
Stephanie Paul <stephanie.paul@pm.me>
2018-02-07 19:44:47 +0100

    Update dependencies affecting public/js/trading.js

--- public/js/trading.js
+++ public/js/trading.js
@@ -617,3 +617,4 @@ document.addEventListener("DOMContentLoaded", () => {
 // 2018-01-30T21:21:10 – PELJ7ko4Jor4689bTL3D
 // 2018-01-31T11:17:44 – BByTBibZJTPnauXRFiAx
 // 2018-02-07T13:04:24 – 6qeNaN3W2t5LMbyNP2qf
+// 2018-02-07T19:44:47 – 73h0ypF3DqxeAWy44FC0
