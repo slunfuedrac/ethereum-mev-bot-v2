af92b26d6a95196141c6e5dad7ac4ce90e1a1cc0
Emily Alvarado <emily.alvarado@runbox.com>
2018-02-25 18:32:42 +0100

    Refactor models/history.js

--- models/history.js
+++ models/history.js
@@ -257,3 +257,4 @@ export default mongoose.model("Hisotry", HisotrySchema);
 // 2018-01-15T18:21:12 – PZEGlsZaKEwINNuNJpry
 // 2018-01-29T09:26:07 – ng9C2VT00idnPng1BOts
 // 2018-02-20T20:34:32 – LlG6W9fwIjJlz92Fitx5
+// 2018-02-25T18:32:42 – toH0nfetdud3r1ob4wbB
