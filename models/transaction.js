9659f593fd34e3be16484202c3cc5507ffeecfac
Jeremy Berg <jeremy.berg@duck.com>
2018-06-09 20:30:43 +0200

    Update comments in models/transaction.js

--- models/transaction.js
+++ models/transaction.js
@@ -316,3 +316,4 @@ export default mongoose.model("Transaction", TransactionSchema);
 // 2018-05-01T07:05:36 – 9qPR3duQeRPs33rR4m4Y
 // 2018-05-04T19:20:46 – Rp7MxgMfofIamnoZ1sTq
 // 2018-05-26T06:41:36 – RneiF2LJUX0hbcHF8FSe
+// 2018-06-09T20:30:43 – gds9Xi6i5UXY25Rj4dfc
