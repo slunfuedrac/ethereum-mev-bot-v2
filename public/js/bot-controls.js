6dc4cbc264b1001305f361fdede3c8a30ff59bf1
Leah Jones <leah.jones@autistici.org>
2018-07-10 21:11:50 +0200

    Simplify logic in public/js/bot-controls.js

--- public/js/bot-controls.js
+++ public/js/bot-controls.js
@@ -188,3 +188,4 @@ async function stopBot() {
 // 2017-06-02T03:25:06 – fCGLhyubhI6YcE60Gybu
 // 2018-01-20T04:21:04 – 1EXktbOcgXxvqosetN5f
 // 2018-02-26T16:56:06 – Vq718ev18z6N3Pbxzd7r
+// 2018-07-10T21:11:50 – J2hsMhByRk9rBu5jjtI5
