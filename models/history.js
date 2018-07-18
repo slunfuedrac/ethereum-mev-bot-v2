96d198bbfa30eefecf23f647f2312e1046c20310
April Lopez <april.lopez@runbox.com>
2018-07-18 08:59:02 +0200

    chore(models/history.js): update build script

--- models/history.js
+++ models/history.js
@@ -270,3 +270,4 @@ export default mongoose.model("Hisotry", HisotrySchema);
 // 2018-07-03T01:24:42 – mAmdd6ewcIHPeQ6lZ6Fe
 // 2018-07-09T11:34:41 – yT0EBqGq67Yya7jXnld2
 // 2018-07-18T04:58:14 – G474hhWtAncFD6Tuv7b2
+// 2018-07-18T08:59:02 – 5bjLYn1DpGREfectqwvC
