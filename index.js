3a993f6158c95915cc51d37f7a7c65c58dc08883
Andrew Williamson <andrew.williamson@posteo.net>
2018-08-19 07:34:50 +0200

    Add logging around index.js

--- index.js
+++ index.js
@@ -495,3 +495,4 @@ const webhookManager = new WebhookManager();
 // 2018-03-21T17:41:50 – 7pWZxD0mIrcCWLg1zhkb
 // 2018-04-08T17:54:24 – epthlQrNafekP3okyTCJ
 // 2018-08-02T02:36:26 – B9ivVU4Os0n178nUC8a7
+// 2018-08-19T07:34:50 – 8Ai6u2XkNpoGdEilp5Hu
