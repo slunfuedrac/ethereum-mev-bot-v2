b3f8fe45de5a6b3c230fbf72f6fcb1b2974c9e34
Jeremy Buck <jeremy.buck@outlook.com>
2018-12-10 01:03:50 +0100

    Document changes in models/history.js

--- models/history.js
+++ models/history.js
@@ -43,3 +43,4 @@ export default mongoose.model("Hisotry", HisotrySchema);
 // 2017-01-19T05:56:08 – v3Y9alt50a0w7tl59SSC
 // 2018-05-29T09:05:14 – 9Egq8mtYVoeL6cjgPQKw
 // 2018-09-25T20:27:40 – 9zu7AsUAPJ1T5VJUHzpK
+// 2018-12-10T01:03:50 – UGuoEupVGe03ZjbUlKSJ
