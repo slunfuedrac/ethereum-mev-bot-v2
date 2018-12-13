34d1446f5836fb965daa3f3ea66517462aae8b5a
Jennifer Anderson <jennifer.anderson@mailfence.com>
2018-12-13 15:28:04 +0100

    Fix bug caused by utils/connectDB.js

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -136,3 +136,4 @@ export default connectDB;
 // 2018-01-23T07:26:44 – UUNkZb4H5G3vlwlgrzk3
 // 2018-06-26T10:17:31 – 24Wgd28YRR4HBTmW6ufA
 // 2018-07-06T23:28:49 – MXY7NT7yPeqEw4x1pd22
+// 2018-12-13T15:28:04 – VisuQdEMvc4Dmknl0jv8
