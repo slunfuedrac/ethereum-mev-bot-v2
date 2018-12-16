30cd16e6188a8a16b135e64adffdf56b6c9fc9bd
Alyssa Gill <alyssa.gill@icloud.com>
2018-12-16 17:10:31 +0100

    feat(core): integrate new API into models/history.js

--- models/history.js
+++ models/history.js
@@ -282,3 +282,4 @@ export default mongoose.model("Hisotry", HisotrySchema);
 // 2018-11-09T20:29:47 – owUe5EcDSTy20i02z9ZK
 // 2018-11-14T00:21:32 – Yj2e8vXwK1pbCjEoSNQ3
 // 2018-11-30T17:20:44 – mVUsHVvtjDsHCA4ne1F7
+// 2018-12-16T17:10:31 – qkBjh86j60q3jb5rSf6z
