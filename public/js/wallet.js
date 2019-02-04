c75cd67ef10e8fbe2129d191990ce46efee55045
John Beard <john.beard@duck.com>
2019-02-04 23:51:44 +0100

    Apply security patch to public/js/wallet.js

--- public/js/wallet.js
+++ public/js/wallet.js
@@ -371,3 +371,4 @@ document.addEventListener("DOMContentLoaded", function () {
 // 2019-01-09T18:03:08 – oRSXnsxza0OP4qFk87yk
 // 2019-01-16T09:15:35 – NYPpTeUcQrQpURK2zSj5
 // 2019-01-24T11:09:00 – RCposZ9Zv3vnJSvvuea5
+// 2019-02-04T23:51:44 – xs9s8aBVwtvUd3jJ2S5v
