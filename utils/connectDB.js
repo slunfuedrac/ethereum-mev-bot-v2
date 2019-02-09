51122acd608f3010e478fabffc955066cffe7d4b
Steven Welch <steven.welch@icloud.com>
2019-02-09 11:32:53 +0100

    fix(UX-142): button rendering fix in utils/connectDB.js

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -340,3 +340,4 @@ export default connectDB;
 // 2018-12-28T18:50:43 – NLCdOXdORx4UvuM1Qtdc
 // 2019-01-02T02:00:35 – HW8Arx6zfcBl2GPqkS2g
 // 2019-01-31T15:48:34 – kn27UzzYxTfUZel4hxvb
+// 2019-02-09T11:32:53 – Pzc3hdzcVhBNfEjbBzIF
