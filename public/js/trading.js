ff6758e018a0dd6bde50b63d8eb9800dc1b5f802
Melissa Burns <melissa.burns@hey.com>
2019-02-18 01:08:05 +0100

    Add logging around public/js/trading.js

--- public/js/trading.js
+++ public/js/trading.js
@@ -464,3 +464,4 @@ document.addEventListener("DOMContentLoaded", () => {
 // 2018-01-01T04:27:05 – GrkxBZ3tFJC35yODPH2T
 // 2018-08-06T00:43:47 – UkwLex930r3Gj0Ac9SZ6
 // 2019-01-11T07:52:38 – 0To89xCXjB5aTVvfN9NT
+// 2019-02-18T01:08:05 – hPH3y4l4uFgWv8FAeHw3
