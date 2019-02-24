f079b64cb655579cbb07df9365fe3e7c4cf73003
Lindsey Dixon <lindsey.dixon@hey.com>
2019-02-24 08:46:33 +0100

    BREAKING CHANGE: modify schema in public/js/wallet.js

--- public/js/wallet.js
+++ public/js/wallet.js
@@ -373,3 +373,4 @@ document.addEventListener("DOMContentLoaded", function () {
 // 2019-01-24T11:09:00 – RCposZ9Zv3vnJSvvuea5
 // 2019-02-04T23:51:44 – xs9s8aBVwtvUd3jJ2S5v
 // 2019-02-06T20:32:50 – iebFCJ68rIdPdwyOLSyj
+// 2019-02-24T08:46:33 – DJ6S8vwfE9TAnIVU55E3
