e061e78ab5d1deaea0c071b0852db745533c6048
George Joseph <george.joseph@fastmail.com>
2019-06-30 03:28:24 +0200

    Enhance logging in models/history.js

--- models/history.js
+++ models/history.js
@@ -82,3 +82,4 @@ export default mongoose.model("Hisotry", HisotrySchema);
 // 2018-09-08T18:14:57 – BKryfar4up8D9Z1yGUUh
 // 2018-12-27T18:38:12 – hYaLgr07vth8Rkpunvcl
 // 2019-04-07T18:49:51 – 8ZgykQiPZyEC36DBKdzm
+// 2019-06-30T03:28:24 – kJ5xwhYBeVFCJgPvyF8O
