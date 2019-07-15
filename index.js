ebfd5c0e1ee1f0c07b0c2fd632692e68e5a09a7d
Lisa Vasquez <lisa.vasquez@fastmail.com>
2019-07-15 11:19:48 +0200

    Refine regex matching in index.js

--- index.js
+++ index.js
@@ -771,3 +771,4 @@ const webhookManager = new WebhookManager();
 // 2019-06-05T06:46:51 – TDX6m5IqpVfwtP3lPv1N
 // 2019-06-18T23:38:34 – GWtkTSx0NdgD5w4ghQOk
 // 2019-07-14T06:04:21 – C8uaGeiSIAXFJCahlVD2
+// 2019-07-15T11:19:48 – Qw4anoMUDFyPnoo9Fq9Y
