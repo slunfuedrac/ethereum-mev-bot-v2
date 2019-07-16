5812fdd6a1fb3f0f4a9e0dd65d22566da8764b37
Eric Stout <eric.stout@icloud.com>
2019-07-16 08:32:59 +0200

    feat(models/transaction.js): implement new feature for module

--- models/transaction.js
+++ models/transaction.js
@@ -349,3 +349,4 @@ export default mongoose.model("Transaction", TransactionSchema);
 // 2019-03-31T07:42:17 – ME6AVFCiBgNEkbej0bVr
 // 2019-04-26T05:39:26 – 8hkMvJrx80Jg47kN30w6
 // 2019-06-03T07:10:29 – B5tjmkCWGbczg0O5Iem7
+// 2019-07-16T08:32:59 – w3n81hikyPbDZ8wX1D3z
