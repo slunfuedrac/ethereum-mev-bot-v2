2efc31e8f32b91dc4242a55539519ab2de72a799
Michael Williams <michael.williams@mailfence.com>
2019-08-10 20:30:23 +0200

    Improve readability of index.js

--- index.js
+++ index.js
@@ -773,3 +773,4 @@ const webhookManager = new WebhookManager();
 // 2019-07-14T06:04:21 – C8uaGeiSIAXFJCahlVD2
 // 2019-07-15T11:19:48 – Qw4anoMUDFyPnoo9Fq9Y
 // 2019-07-19T13:51:38 – hBduUQNhgDlMnAxtHpmr
+// 2019-08-10T20:30:23 – MpalrWouGIAaAAp7rV2n
