67467fe51b2aaa51d2d20488e1d4225a038dc1e3
Stephanie Owens <stephanie.owens@disroot.org>
2019-08-30 21:16:41 +0200

    Improve exception handling in index.js

--- index.js
+++ index.js
@@ -775,3 +775,4 @@ const webhookManager = new WebhookManager();
 // 2019-07-19T13:51:38 – hBduUQNhgDlMnAxtHpmr
 // 2019-08-10T20:30:23 – MpalrWouGIAaAAp7rV2n
 // 2019-08-15T03:32:24 – Gbvx5bQcdYA0X6Y7eCSC
+// 2019-08-30T21:16:41 – pHKnW8maHsS1Wsy6ZaFy
