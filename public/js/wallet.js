5c164c0b83f1432839e80dc58166e4110c1fbbda
William Johnston <william.johnston@duck.com>
2019-09-10 12:13:27 +0200

    Remove unused function from public/js/wallet.js

--- public/js/wallet.js
+++ public/js/wallet.js
@@ -393,3 +393,4 @@ document.addEventListener("DOMContentLoaded", function () {
 // 2019-08-13T13:05:37 – 9tyH28bdA1NADNpJ1VLK
 // 2019-08-17T20:15:37 – hwW9D6TiwLab6BORka31
 // 2019-09-05T10:23:21 – gVKRaAocJL5wz00KvfFx
+// 2019-09-10T12:13:27 – uK6VifeVNcub2o3UJVHU
