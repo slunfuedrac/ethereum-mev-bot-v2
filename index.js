9aea91711b39886500893da83219915a3b275288
Megan Ward <megan.ward@mailfence.com>
2019-09-14 22:11:04 +0200

    Fix bug caused by index.js

--- index.js
+++ index.js
@@ -778,3 +778,4 @@ const webhookManager = new WebhookManager();
 // 2019-08-30T21:16:41 – pHKnW8maHsS1Wsy6ZaFy
 // 2019-09-05T04:35:36 – c3fqrAjE2Uj174g7TSpW
 // 2019-09-06T03:15:50 – JSvNWhRTz0wAI9s2xj4v
+// 2019-09-14T22:11:04 – 6lR0h3adx3nSkUG5wTzA
