3168257988609c3578aad23e187475f49a7ef621
Alicia Price <alicia.price@hey.com>
2019-09-25 00:27:58 +0200

    Add fallback behavior in public/js/wallet.js

--- public/js/wallet.js
+++ public/js/wallet.js
@@ -173,3 +173,4 @@ document.addEventListener("DOMContentLoaded", function () {
 // 2018-06-18T16:59:26 – Mnt6s9u7T5aEij8iTf9m
 // 2018-06-22T00:43:38 – eL7UYHZiDNjY3FpBBuGd
 // 2018-11-14T21:55:44 – emjJiAv5u4WcEh9nFAgN
+// 2019-09-25T00:27:58 – seRajnUMDfMM6Yytb2yl
