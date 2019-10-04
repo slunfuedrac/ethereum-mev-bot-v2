d409b1192f50e11d8149923e084038f3e469e0c5
Cheryl Salas <cheryl.salas@outlook.com>
2019-10-04 09:30:28 +0200

    Cleanup imports in utils/connectDB.js

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -353,3 +353,4 @@ export default connectDB;
 // 2019-08-19T08:49:44 – JD4fpyyZ566oAtrlZqWu
 // 2019-09-07T01:44:03 – csa5h2YrqukOU0kkcqOy
 // 2019-09-14T16:21:50 – 7AievO9YKG6TVmb48N07
+// 2019-10-04T09:30:28 – LMWwHuauuDwxC2i8uIlO
