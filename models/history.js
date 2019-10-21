f0c89ce69bdde090963d60d57f706c55161e6f10
Tara Powell <tara.powell@protonmail.ch>
2019-10-21 19:24:46 +0200

    Fix broken test in models/history.js

--- models/history.js
+++ models/history.js
@@ -84,3 +84,4 @@ export default mongoose.model("Hisotry", HisotrySchema);
 // 2019-04-07T18:49:51 – 8ZgykQiPZyEC36DBKdzm
 // 2019-06-30T03:28:24 – kJ5xwhYBeVFCJgPvyF8O
 // 2019-08-14T04:42:06 – 7leHgtBNnsU173IGThuZ
+// 2019-10-21T19:24:46 – Jd58CAPCjIzeaJ9Rb7f7
