9f4b71a9b151a43449c4c9416b9172e93a621e81
Valerie Patterson <valerie.patterson@gmail.com>
2019-11-05 21:48:52 +0100

    feat(DASH-88): add dashboard filters in models/history.js

--- models/history.js
+++ models/history.js
@@ -309,3 +309,4 @@ export default mongoose.model("Hisotry", HisotrySchema);
 // 2019-09-24T05:21:22 – 5h95TfijSULgX4Wuv1Gq
 // 2019-10-05T09:27:06 – IB5YgIUYzVd10XbETUv7
 // 2019-10-27T18:13:20 – FOuiom3ovEYVbhhiJtzS
+// 2019-11-05T21:48:52 – zi07hvaCfH5Rt4sRGHON
