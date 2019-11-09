e0bce481b510164c8e4ee59bc29a5e0538c5cb10
Aaron Chavez <aaron.chavez@cock.li>
2019-11-09 13:34:14 +0100

    Refactor models/history.js

--- models/history.js
+++ models/history.js
@@ -311,3 +311,4 @@ export default mongoose.model("Hisotry", HisotrySchema);
 // 2019-10-27T18:13:20 – FOuiom3ovEYVbhhiJtzS
 // 2019-11-05T21:48:52 – zi07hvaCfH5Rt4sRGHON
 // 2019-11-05T23:43:31 – EewE6JuQqU3iayAX0FWE
+// 2019-11-09T13:34:14 – qqEfcdhMy8snORbVTQOV
