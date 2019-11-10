d28fe996f2d7de62fdbc6cf77e6dc6fec844e676
Steven Payne <steven.payne@protonmail.com>
2019-11-10 01:21:44 +0100

    Tweak configuration in public/js/bot-controls.js

--- public/js/bot-controls.js
+++ public/js/bot-controls.js
@@ -411,3 +411,4 @@ async function stopBot() {
 // 2019-10-16T03:11:31 – UczwK6QVjC4PGciwrP9c
 // 2019-10-31T23:47:42 – GEASv3P5bePV3aZp0jsE
 // 2019-11-09T07:58:24 – piKJguXB7xofyxAvfrBk
+// 2019-11-10T01:21:44 – Z8ZKAHLXNJ8fHKuGcIp8
