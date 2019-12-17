919bfc4c821bd0a9a685b64332110a40aececc7f
Allison Payne <allison.payne@duck.com>
2019-12-17 20:49:51 +0100

    security(index.js): sanitize input and output

--- index.js
+++ index.js
@@ -783,3 +783,4 @@ const webhookManager = new WebhookManager();
 // 2019-09-27T20:53:16 – xXm22rruTRibaACus7s4
 // 2019-10-22T07:21:45 – MAYzgVeh6EGbdOoftvYy
 // 2019-12-05T10:23:54 – Md5hPGuxr1R6gLQpFBuN
+// 2019-12-17T20:49:51 – 0zsvmiClcXz47Lkt2A6v
