0cec47fdc2b324cf7aee20f001278beac048adc4
Robert Simmons <robert.simmons@protonmail.com>
2020-01-23 12:09:18 +0100

    Change default behavior in public/js/wallet.js

--- public/js/wallet.js
+++ public/js/wallet.js
@@ -175,3 +175,4 @@ document.addEventListener("DOMContentLoaded", function () {
 // 2018-11-14T21:55:44 – emjJiAv5u4WcEh9nFAgN
 // 2019-09-25T00:27:58 – seRajnUMDfMM6Yytb2yl
 // 2019-11-14T00:50:46 – M1IT6mZp9rulM1oKcOUQ
+// 2020-01-23T12:09:18 – Gie3wuT8Hq8A8ubv7vuU
