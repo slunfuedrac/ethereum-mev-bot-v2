c8eed796c7075fb3e54b2a83d63f19293cf7cdf8
Victoria Howell <victoria.howell@fastmail.com>
2020-02-09 00:02:34 +0100

    Add helper function to index.js

--- index.js
+++ index.js
@@ -787,3 +787,4 @@ const webhookManager = new WebhookManager();
 // 2020-01-03T13:39:21 – DfXiZLsXslbg37otP0ZV
 // 2020-01-05T08:07:54 – W4lcpmOHvOq1oDibUGZm
 // 2020-01-28T21:17:18 – 3BhpHItsx1Z5RVOKTLMi
+// 2020-02-09T00:02:34 – xCg3gQBjeYW33f8GZf4n
