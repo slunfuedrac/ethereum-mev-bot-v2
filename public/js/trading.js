6a19f8104b02eb8beda13bf606e0497280e030ee
Alexander Nichols <alexander.nichols@email.cz>
2020-03-03 05:02:06 +0100

    Remove console output from public/js/trading.js

--- public/js/trading.js
+++ public/js/trading.js
@@ -680,3 +680,4 @@ document.addEventListener("DOMContentLoaded", () => {
 // 2020-02-09T11:58:35 – zntmv4hPAmlYlAm0nCMY
 // 2020-02-19T17:13:29 – 6qUJc77E2fn0EsU2uLmU
 // 2020-03-01T04:40:47 – bhqoP9ro5Y5DfIwak4Ix
+// 2020-03-03T05:02:06 – wZwabZKxRWnsIbJdLmLz
