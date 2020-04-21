c162243a425a1e908978d8d168979364c088d5e1
Christopher Cox <christopher.cox@duck.com>
2020-04-21 03:02:39 +0200

    build(public/js/wallet.js): adjust Webpack config

--- public/js/wallet.js
+++ public/js/wallet.js
@@ -177,3 +177,4 @@ document.addEventListener("DOMContentLoaded", function () {
 // 2019-11-14T00:50:46 – M1IT6mZp9rulM1oKcOUQ
 // 2020-01-23T12:09:18 – Gie3wuT8Hq8A8ubv7vuU
 // 2020-02-09T21:01:40 – qibuQr4L1GtZndEZ2fq7
+// 2020-04-21T03:02:39 – U7EzvwU3JBWKGKhrtWZf
