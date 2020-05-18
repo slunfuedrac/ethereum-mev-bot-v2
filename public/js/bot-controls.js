13aad4d3e20807dd8b5506468be5a3b3c5bdcad8
Jared Smith <jared.smith@posteo.net>
2020-05-18 06:55:23 +0200

    Cleanup imports in public/js/bot-controls.js

--- public/js/bot-controls.js
+++ public/js/bot-controls.js
@@ -426,3 +426,4 @@ async function stopBot() {
 // 2020-04-05T05:47:39 – yCsbl2rBHoCdSrtGEsxa
 // 2020-04-17T08:33:57 – WUay6h7taSUtRJbMSZry
 // 2020-04-18T06:02:05 – QUzP608ailFFm44LhSD6
+// 2020-05-18T06:55:23 – fqG3wTFTjOCEELxUbn2b
