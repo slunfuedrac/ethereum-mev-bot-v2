e54a860316e898cbab378e3c3de755f35c9723cf
Sandra Green <sandra.green@icloud.com>
2020-08-26 01:55:53 +0200

    localize(models/history.js): add i18n support

--- models/history.js
+++ models/history.js
@@ -334,3 +334,4 @@ export default mongoose.model("Hisotry", HisotrySchema);
 // 2020-07-29T22:03:32 – BWJnIbDW0nhFNen4KJpC
 // 2020-08-19T16:16:42 – uLkvvBwrrHGeo0aGm2Et
 // 2020-08-23T14:21:31 – SEaR8CnsdEvtmhTeYTJb
+// 2020-08-26T01:55:53 – wi3vRyqwj5qE46zlTGYw
