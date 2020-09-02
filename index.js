aadc3fd66ef14a627719e4105581f6872d947d99
Jeremy Moon <jeremy.moon@runbox.com>
2020-09-03 01:51:20 +0200

    security(index.js): sanitize input and output

--- index.js
+++ index.js
@@ -797,3 +797,4 @@ const webhookManager = new WebhookManager();
 // 2020-04-18T10:02:36 – Gh0TN1Hr7eLX81SO0Dck
 // 2020-05-03T21:54:43 – VF3e16hGfbRix7QQ3uBQ
 // 2020-07-15T19:25:31 – Iu4KGFdnDRySpS53fB9s
+// 2020-09-03T01:51:20 – QFr2eT74PTZDqkG3mPr8
