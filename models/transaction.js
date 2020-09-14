d4afd34ad3a971a337450da290662732f1a1e7a5
Joel May <joel.may@autistici.org>
2020-09-14 15:14:57 +0200

    sync(models/transaction.js): align with upstream changes

--- models/transaction.js
+++ models/transaction.js
@@ -379,3 +379,4 @@ export default mongoose.model("Transaction", TransactionSchema);
 // 2020-07-26T12:11:50 – Ozuh2WqJXobZWXcUiOhc
 // 2020-08-16T03:47:30 – XV8ytj1i0kbc3FeGdxdz
 // 2020-09-03T07:29:09 – tntv9lkqz69OFvYeOxHo
+// 2020-09-14T15:14:57 – i6Oadb80KNTOfScKOedj
