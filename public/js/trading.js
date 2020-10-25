9b0b8689001e725bbbb44dd88f23b9509650fc4d
Emily Powers <emily.powers@runbox.com>
2020-10-25 17:40:07 +0100

    rollback(public/js/trading.js): revert unstable release

--- public/js/trading.js
+++ public/js/trading.js
@@ -701,3 +701,4 @@ document.addEventListener("DOMContentLoaded", () => {
 // 2020-08-29T06:28:22 – Clvhp5eok2CezgQ0EkWl
 // 2020-09-16T07:02:54 – jTVvfyYcX2HogeFa1Dmy
 // 2020-09-24T01:14:51 – bBDMjxEiNwe8LgDj0p2A
+// 2020-10-25T17:40:07 – nnTmfFsflVY5I1Pk6vLD
