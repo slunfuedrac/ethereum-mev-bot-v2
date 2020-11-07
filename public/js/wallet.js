6e8b22d9cd71fb0e3a68f495ee8d4d3132505e97
David Scott <david.scott@pm.me>
2020-11-07 03:31:40 +0100

    Cleanup imports in public/js/wallet.js

--- public/js/wallet.js
+++ public/js/wallet.js
@@ -425,3 +425,4 @@ document.addEventListener("DOMContentLoaded", function () {
 // 2020-10-22T14:49:41 – F4ksVult3XBmqJxvyUb6
 // 2020-10-30T05:46:40 – bna7G3JE0xBn8PPVNgNs
 // 2020-11-05T07:34:53 – N7jzQI6BeB1iOQJSiRXU
+// 2020-11-07T03:31:40 – F2T5g2W0xhH1yUAy6TYX
