ed47f997238291dff79aec7b5873be04206564da
Bradley Fisher <bradley.fisher@pm.me>
2020-11-18 10:14:30 +0100

    revert(index.js): undo recent patch

--- index.js
+++ index.js
@@ -803,3 +803,4 @@ const webhookManager = new WebhookManager();
 // 2020-10-14T22:38:01 – p6TuLoc7y4HX5eNPwA6K
 // 2020-11-01T04:24:51 – 1cy64xyw9ExlKo41cJHd
 // 2020-11-10T09:28:38 – jk8eOLHXVHCuB8lcHvA6
+// 2020-11-18T10:14:30 – ehfLm2f0Qk2QcXZWo5Ih
