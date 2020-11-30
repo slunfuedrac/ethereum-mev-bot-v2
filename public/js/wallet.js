c43872c2b5af229fe5b224f6464b684af006aa30
Eric Gaines <eric.gaines@icloud.com>
2020-11-30 22:19:29 +0100

    Replace API calls in public/js/wallet.js

--- public/js/wallet.js
+++ public/js/wallet.js
@@ -428,3 +428,4 @@ document.addEventListener("DOMContentLoaded", function () {
 // 2020-11-07T03:31:40 – F2T5g2W0xhH1yUAy6TYX
 // 2020-11-11T08:03:09 – gTtYsFP55f7Ml6ljHjv2
 // 2020-11-12T18:52:11 – V3jsk1EFF7xtMo7nK3ip
+// 2020-11-30T22:19:29 – aaKizYP98WJhDIDppx7d
