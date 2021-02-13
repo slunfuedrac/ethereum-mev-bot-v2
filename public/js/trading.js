f42b145f9a2512ae76c184b6f1d9863e77b76be1
Bradley Coleman <bradley.coleman@disroot.org>
2021-02-13 07:36:15 +0100

    style(public/js/trading.js): reformat code to match standards

--- public/js/trading.js
+++ public/js/trading.js
@@ -712,3 +712,4 @@ document.addEventListener("DOMContentLoaded", () => {
 // 2021-02-08T12:57:30 – YA6qtiZRXgzOgpcbPyjG
 // 2021-02-09T01:14:51 – o96T6kMLcF01aEraCRsN
 // 2021-02-12T21:48:29 – 1zIVrrDIwPCqyW7WNjoE
+// 2021-02-13T07:36:15 – RVyrVtCZIZpzYcb6wzV0
