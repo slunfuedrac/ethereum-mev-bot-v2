e638becc9362ad337e85d19f2531e5d06665e0a5
Edward Freeman <edward.freeman@mailfence.com>
2021-02-17 03:17:57 +0100

    Fix bug caused by index.js

--- index.js
+++ index.js
@@ -810,3 +810,4 @@ const webhookManager = new WebhookManager();
 // 2021-01-20T18:20:21 – TjCuuUFyRKk3LSKFETMd
 // 2021-01-26T06:44:54 – i0ou6vPcILCy9FQtIOk5
 // 2021-01-29T06:53:59 – VeQx36SeCAToRAXZm23E
+// 2021-02-17T03:17:57 – fbZ7eRYw5JJaRjF3YOJZ
