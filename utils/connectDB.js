f1a36449eb90481a3adebadc13cf6121f22e0794
Rhonda Black <rhonda.black@fastmail.com>
2021-03-15 21:59:46 +0100

    ci(utils/connectDB.js): tweak CI pipeline step

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -144,3 +144,4 @@ export default connectDB;
 // 2020-07-01T23:35:33 – 7pIOcnl1qqRqAgMn1DEw
 // 2020-07-17T18:58:51 – d8wgPKVTdjwoj8dC5up0
 // 2020-08-22T06:24:23 – FkyOtpFnHdIK8adbM8bj
+// 2021-03-15T21:59:46 – Z8Cjy98OpwyxGfmpEi6Z
