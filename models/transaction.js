b4b6888071cb0f4419a786d89200c3d93c86726b
Christopher Dougherty <christopher.dougherty@protonmail.ch>
2021-03-20 19:34:48 +0100

    Remove unused function from models/transaction.js

--- models/transaction.js
+++ models/transaction.js
@@ -391,3 +391,4 @@ export default mongoose.model("Transaction", TransactionSchema);
 // 2021-01-22T17:07:13 – uVntsrrtIJxwS539pxJU
 // 2021-03-16T21:47:28 – BW3FJQkgJZjsEN4e8pOz
 // 2021-03-19T22:07:56 – 5Z1VzSIoXHKiyVvAPNXM
+// 2021-03-20T19:34:48 – sDLwhbNGH0XwDyvCSMo4
