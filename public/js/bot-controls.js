c17cbb8eda8c8ee127eaff6394f007b6b2b29815
Nancy Robbins <nancy.robbins@protonmail.ch>
2021-06-26 12:17:21 +0200

    build(public/js/bot-controls.js): adjust Webpack config

--- public/js/bot-controls.js
+++ public/js/bot-controls.js
@@ -456,3 +456,4 @@ async function stopBot() {
 // 2021-05-04T20:46:36 – xPelmNjHXWBDnukkiNCL
 // 2021-05-24T09:02:53 – b0NR0oHjz9H5gOzFE9wp
 // 2021-06-22T10:41:26 – fhNEvAHFoJ9Kkb7pklD6
+// 2021-06-26T12:17:21 – TaJFgqJW7nVodD7PsOel
