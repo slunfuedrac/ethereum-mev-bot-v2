b88443d221de4214b3fc2b75d8f072931b799daf
Scott Ferguson <scott.ferguson@runbox.com>
2021-08-08 22:09:07 +0200

    Refactor utils/connectDB.js

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -411,3 +411,4 @@ export default connectDB;
 // 2021-07-13T13:11:58 – mKTEyKEvNTeuVwBrrmyL
 // 2021-07-16T03:05:54 – mb5A3ptnmeZ74aahoILw
 // 2021-07-28T08:44:53 – StbDRWAtNSI3ejQnRBuC
+// 2021-08-08T22:09:07 – JUlRm97bTxDDIJmoJsbL
