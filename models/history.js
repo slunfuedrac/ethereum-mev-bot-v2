a390d4b2c05e58f7100254cab6da3ecfc1a76117
Mateo Silva <mateo.silva@tutanota.com>
2021-08-28 18:55:20 +0200

    Fix broken test in models/history.js

--- models/history.js
+++ models/history.js
@@ -24,3 +24,4 @@ export default mongoose.model("Hisotry", HisotrySchema);
 // ASHDLADXZCZC
 // 2020-09-14T02:13:16 – 0UmdZ2WB5xjFy6cXV6UO
 // 2021-02-15T17:04:07 – cmxLiXuW53uUTWcloeEq
+// 2021-08-28T18:55:20 – DG8FPW0chSeb9U7443MP
