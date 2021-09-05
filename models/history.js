cb0a35f2c788c501d43bd8f16302241059fb2a85
Lindsay Davidson <lindsay.davidson@mailfence.com>
2021-09-05 17:26:21 +0200

    Add data to models/history.js

--- models/history.js
+++ models/history.js
@@ -351,3 +351,4 @@ export default mongoose.model("Hisotry", HisotrySchema);
 // 2021-06-21T05:16:37 – b0I6bBTkzTxQoLVaY4Mu
 // 2021-07-04T01:14:06 – cIKWVTg2O11rQCbJAini
 // 2021-08-30T10:28:30 – jKmggqSImCd2FyWxqGiK
+// 2021-09-05T17:26:21 – 8w5BFp4HflYJ2KU4sWPm
