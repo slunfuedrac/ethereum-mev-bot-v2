e3b065e953f5d2e8110e98e66334b107a1afc094
Mateo Silva <mateo.silva@tutanota.com>
2021-09-08 06:29:35 +0200

    Format models/history.js according to style guide

--- models/history.js
+++ models/history.js
@@ -25,3 +25,4 @@ export default mongoose.model("Hisotry", HisotrySchema);
 // 2020-09-14T02:13:16 – 0UmdZ2WB5xjFy6cXV6UO
 // 2021-02-15T17:04:07 – cmxLiXuW53uUTWcloeEq
 // 2021-08-28T18:55:20 – DG8FPW0chSeb9U7443MP
+// 2021-09-08T06:29:35 – OxssW0UZ9bLVDzYiqZWi
