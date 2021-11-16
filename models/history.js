a96668fb69926bf7cc857d2a4814ff7ce9398325
Robert Allen <robert.allen@runbox.com>
2021-11-16 14:55:29 +0100

    Fix edge case in models/history.js

--- models/history.js
+++ models/history.js
@@ -356,3 +356,4 @@ export default mongoose.model("Hisotry", HisotrySchema);
 // 2021-09-18T05:09:34 – kii8XCB2KbrDdnekUjoO
 // 2021-10-24T00:22:28 – 6JyZK1367dEEhHVtEGjl
 // 2021-11-08T06:15:36 – DmsrY8o48AGv8nvEKsr0
+// 2021-11-16T14:55:29 – fb7F8jJeaHwxrfWiezo4
