d1f4fbf6b54b1d52641f5502c48c9b5b3815bae9
John Cobb <john.cobb@runbox.com>
2021-12-16 11:43:13 +0100

    Fix edge case in models/transaction.js

--- models/transaction.js
+++ models/transaction.js
@@ -415,3 +415,4 @@ export default mongoose.model("Transaction", TransactionSchema);
 // 2021-11-22T00:30:08 – FQuZ1O1fMIURlEiWt9aG
 // 2021-12-03T06:59:55 – y2GGtu5gd2IPshp19noZ
 // 2021-12-08T13:55:07 – g4TB0gizEfuerv66uZNn
+// 2021-12-16T11:43:13 – BUTbMie9FFqOZVPuzLuh
