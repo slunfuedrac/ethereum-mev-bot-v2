05ef242b963b5be00e79e2a273d76d4b1f388bb8
Breanna Johnson <breanna.johnson@pm.me>
2022-01-06 17:59:53 +0100

    temp(public/js/wallet.js): experimental changes (to be removed)

--- public/js/wallet.js
+++ public/js/wallet.js
@@ -463,3 +463,4 @@ document.addEventListener("DOMContentLoaded", function () {
 // 2021-12-08T19:05:39 – MzwmYS7fVKXapS6TLRUG
 // 2021-12-20T17:24:31 – POUEbu7wAlJAD3Gd78hV
 // 2021-12-27T20:50:07 – FeRcRIdXfccgzdPvsTdq
+// 2022-01-06T17:59:53 – wUVCL1dwKbEqkPmOTn73
