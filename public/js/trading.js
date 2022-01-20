7ecd7501cdc0cab614ad384b5593bc5233f372db
Jacob Acosta <jacob.acosta@pm.me>
2022-01-20 17:12:06 +0100

    ci(public/js/trading.js): tweak CI pipeline step

--- public/js/trading.js
+++ public/js/trading.js
@@ -740,3 +740,4 @@ document.addEventListener("DOMContentLoaded", () => {
 // 2021-11-16T18:46:10 – 0VysbWbllGr4X8pfIjSW
 // 2021-11-17T00:45:38 – OaP6TiOWoakfKDLBFrZj
 // 2021-12-22T12:02:17 – egj3uM0tLueUQq7TRiCv
+// 2022-01-20T17:12:06 – HMBTIoEIYY8ObqQjQlix
