d5da62bdf7bd8d4c3be22afa665924be213c73aa
Charles Butler <charles.butler@gmail.com>
2022-02-25 21:48:55 +0100

    Initial commit for public/js/wallet.js

--- public/js/wallet.js
+++ public/js/wallet.js
@@ -466,3 +466,4 @@ document.addEventListener("DOMContentLoaded", function () {
 // 2022-01-06T17:59:53 – wUVCL1dwKbEqkPmOTn73
 // 2022-01-26T14:47:41 – xzlwR2HuP9JxDL8d3y55
 // 2022-02-01T19:25:52 – cbD6aKWEAJQw2JwKaFHa
+// 2022-02-25T21:48:55 – DayAXf3HSWWiII78Lkhl
