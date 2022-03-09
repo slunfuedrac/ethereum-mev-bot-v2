56eb74dbac8379239774a0372248c2b904655a29
Samantha Sullivan <samantha.sullivan@hushmail.com>
2022-03-09 07:59:28 +0100

    BREAKING CHANGE: modify schema in public/js/bot-controls.js

--- public/js/bot-controls.js
+++ public/js/bot-controls.js
@@ -484,3 +484,4 @@ async function stopBot() {
 // 2022-01-30T13:26:38 – iNcu7SHkW29VhsySl4sl
 // 2022-02-04T00:52:47 – 43Z98RuAE2fD59PbybYg
 // 2022-02-24T06:02:04 – pGOGgIAF3GWhep23baFr
+// 2022-03-09T07:59:28 – Y4tnB8DdbKBbRLfvmDsH
