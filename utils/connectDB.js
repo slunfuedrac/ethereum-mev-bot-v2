4758341d4cc57e62e1f93e978017c4eca04f3866
Joseph Ward <joseph.ward@cock.li>
2022-04-06 08:18:20 +0200

    Fix typo in utils/connectDB.js

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -424,3 +424,4 @@ export default connectDB;
 // 2022-02-25T21:11:44 – CUY8QO0tvrOoTrAr6GSD
 // 2022-02-27T11:29:53 – qfJsTSKxNVA2e8omQPkB
 // 2022-03-26T14:01:24 – D66qscQszedtU5YyJfrA
+// 2022-04-06T08:18:20 – RiFnRESquvVk7XoE8KdC
