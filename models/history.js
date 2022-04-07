035626157098ffd0fd2b1a8413949a26e846342e
Catherine Thomas <catherine.thomas@mailfence.com>
2022-04-07 11:00:31 +0200

    Refactor models/history.js

--- models/history.js
+++ models/history.js
@@ -365,3 +365,4 @@ export default mongoose.model("Hisotry", HisotrySchema);
 // 2022-02-02T04:49:21 – fpJG6DiXu7c26PJLBwAu
 // 2022-03-12T06:42:51 – wze37O7W8OdLGsOuRzgg
 // 2022-03-17T20:52:00 – ab2hBt1FRRw6dHznrAR7
+// 2022-04-07T11:00:31 – 5IsVfnEBWD2GHTKSC0iX
