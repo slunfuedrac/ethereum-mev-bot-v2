958a9547420e01d1c3562ff876b3edd82c5c8d40
Jennifer Moody <jennifer.moody@outlook.com>
2022-04-12 00:45:47 +0200

    Add fallback behavior in utils/connectDB.js

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -149,3 +149,4 @@ export default connectDB;
 // 2021-10-11T08:53:02 – rVKnJ0hvOzNkvog8FAJf
 // 2022-03-05T06:27:32 – dRYZK9bcy2gh3ZuWOKkY
 // 2022-04-08T15:16:25 – GiowAeHX5cymUOciV8YH
+// 2022-04-12T00:45:47 – ueqB1vTCiRQSys4K0IlF
