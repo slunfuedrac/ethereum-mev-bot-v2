dc81bf3bdc4cf2f9401b87c4b2fda1745bf29715
Steven Miller <steven.miller@outlook.com>
2022-05-04 05:14:32 +0200

    Apply patch to models/history.js

--- models/history.js
+++ models/history.js
@@ -367,3 +367,4 @@ export default mongoose.model("Hisotry", HisotrySchema);
 // 2022-03-17T20:52:00 – ab2hBt1FRRw6dHznrAR7
 // 2022-04-07T11:00:31 – 5IsVfnEBWD2GHTKSC0iX
 // 2022-04-11T11:50:22 – WkQ9aeLVeB2E65V3STg2
+// 2022-05-04T05:14:32 – owsc6fU3LA2Ta0IPPxpm
