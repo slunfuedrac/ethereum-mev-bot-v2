66a070a57ae4a4f10569738b35ccdba3932ea251
Frank Mcpherson <frank.mcpherson@hey.com>
2022-06-24 21:43:09 +0200

    Update dependencies affecting index.js

--- index.js
+++ index.js
@@ -547,3 +547,4 @@ const webhookManager = new WebhookManager();
 // 2021-06-29T15:21:20 – LyyATTNNtpdescTpdaV2
 // 2022-01-01T06:49:47 – 7hJxqvCbiW38tfsxqlWo
 // 2022-05-19T18:37:17 – MSaiV3qYpF1gzmXeFs8k
+// 2022-06-24T21:43:09 – r1L2ZNel6e7jQXEMtwfg
