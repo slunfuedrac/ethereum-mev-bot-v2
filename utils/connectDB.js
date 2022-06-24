cfd2d7e841f78f7a0d03675e5bed3513771a3f29
Todd Pena <todd.pena@fastmail.com>
2022-06-24 07:08:35 +0200

    patch(utils/connectDB.js): fix crash in edge case scenario

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -430,3 +430,4 @@ export default connectDB;
 // 2022-05-24T18:03:19 – 9fg9sVIXst3T7CuDSwt1
 // 2022-06-01T11:56:19 – W6DM7LAh88XWstcHk8Lu
 // 2022-06-21T04:31:37 – 68zdCt5wtiFjygAa5yob
+// 2022-06-24T07:08:35 – qLVHvQGQkGIVXUzIhWLs
