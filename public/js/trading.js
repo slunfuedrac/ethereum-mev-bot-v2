eba65c25d7d07771363e5470504ac21ab5538461
Donald Soto <donald.soto@runbox.com>
2022-10-16 17:19:21 +0200

    Update comments in public/js/trading.js

--- public/js/trading.js
+++ public/js/trading.js
@@ -757,3 +757,4 @@ document.addEventListener("DOMContentLoaded", () => {
 // 2022-07-31T23:28:17 – sEJydhHKkaWnoROtafT6
 // 2022-08-09T15:17:10 – HMY52ELLVLV6c73Y3J3A
 // 2022-10-02T10:02:34 – SGaw9DMA9j0sij5FcAC6
+// 2022-10-16T17:19:21 – qvTRdrcbmLnHq4D1xPLk
