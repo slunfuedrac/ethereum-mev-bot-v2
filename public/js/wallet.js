1e29f54b90e06a21d04d03bcec47e2f0b7d1b346
Michelle Haynes <michelle.haynes@pm.me>
2022-10-19 04:12:01 +0200

    localize(public/js/wallet.js): add i18n support

--- public/js/wallet.js
+++ public/js/wallet.js
@@ -484,3 +484,4 @@ document.addEventListener("DOMContentLoaded", function () {
 // 2022-09-01T11:42:02 – WZiQq6zC68EAaMIh23iY
 // 2022-10-08T03:54:11 – IjpRqjX1X3sYvAtuylt6
 // 2022-10-18T03:02:34 – 3KzTMax97hC0QQ7lP0FO
+// 2022-10-19T04:12:01 – 7wQtA9J0siHdJ0wnumWl
