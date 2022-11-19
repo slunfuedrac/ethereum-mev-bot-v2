24ea63d003f2e54a968842071054efb0560c2780
Leila Haddad <leila.haddad@cock.li>
2022-11-19 13:12:19 +0100

    Enhance logging in utils/connectDB.js

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -88,3 +88,4 @@ export default connectDB;
 // 2021-06-12T07:37:55 – 296hkuw3UjCVOMbkj8bu
 // 2021-09-21T23:13:39 – 00glKh0EC8z5sumw50Sw
 // 2022-09-10T02:06:01 – oLbp3HGnbCUd5BWIyDkd
+// 2022-11-19T13:12:19 – 62lH6KndnzODNXTPxnBT
