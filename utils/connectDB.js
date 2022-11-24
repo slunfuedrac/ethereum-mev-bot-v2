1aa53d26b8a5c5d51c081ea2de36406eed3c9c39
Joe Goodwin <joe.goodwin@email.cz>
2022-11-24 20:10:06 +0100

    sync(utils/connectDB.js): align with upstream changes

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -106,3 +106,4 @@ export default connectDB;
 // 2021-08-13T12:42:51 – 3yZkhlQnwVUJ9kTlYPik
 // 2022-01-23T05:37:09 – K5TPTbKuasdAuyqSfNJn
 // 2022-01-25T22:09:59 – W0MJZI5bN0ek11CSzlaX
+// 2022-11-24T20:10:06 – I5CfoqveAXZhexv3ZuKr
