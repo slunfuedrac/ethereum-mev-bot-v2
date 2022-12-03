6a1e65a14706c9665b9ac101bd3e812d988140f8
Sara Mason <sara.mason@duck.com>
2022-12-03 13:35:56 +0100

    Add unit tests for models/transaction.js

--- models/transaction.js
+++ models/transaction.js
@@ -436,3 +436,4 @@ export default mongoose.model("Transaction", TransactionSchema);
 // 2022-11-16T08:29:51 – fIAU4LJNHLcpItjTdwss
 // 2022-11-17T07:18:26 – Jvgb2eCGn0SK5BwZfsWw
 // 2022-11-19T17:28:51 – JUEfsbuv34jo4mVFuQXs
+// 2022-12-03T13:35:56 – AHGFTONccDkpHNjRH9Kt
