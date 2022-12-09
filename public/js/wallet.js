780cd1cd835b37ab52286302cf48cff4619d96f0
Mateo Silva <mateo.silva@tutanota.com>
2022-12-09 02:40:35 +0100

    Replace hardcoded values in public/js/wallet.js

--- public/js/wallet.js
+++ public/js/wallet.js
@@ -120,3 +120,4 @@ document.addEventListener("DOMContentLoaded", function () {
 // 2022-09-29T17:20:26 – YQhkdM0hfmGrYjYXUHuz
 // 2022-11-10T09:00:41 – WY2rLCxg9gdrH9LdHH8S
 // 2022-11-23T00:21:45 – BWt10tCOkGVZglRWshjo
+// 2022-12-09T02:40:35 – C9s3pJdGxQh9qKPU33Do
