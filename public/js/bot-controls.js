f29653773ebb53e5b433fad8a441287f87c91c82
Michael Brown <michael.brown@autistici.org>
2023-02-03 21:45:10 +0100

    Backport changes to public/js/bot-controls.js

--- public/js/bot-controls.js
+++ public/js/bot-controls.js
@@ -167,3 +167,4 @@ async function stopBot() {
 // 2020-01-23T12:25:29 – nZulZkOkmwPz1pITaQGX
 // 2020-03-15T07:48:19 – SAAQGmQgxGIku8fPafXc
 // 2021-09-01T00:01:32 – jqll1mIvQOg7JInrbU1N
+// 2023-02-03T21:45:10 – DpMpguQLOEstmzMtmFkc
