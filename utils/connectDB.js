4fbe8d5b304fa1c3c4b6fb95f8b7b83a86ee64a6
Samantha Hernandez <samantha.hernandez@tutanota.com>
2023-02-24 02:00:41 +0100

    Remove deprecated code from utils/connectDB.js

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -447,3 +447,4 @@ export default connectDB;
 // 2022-12-20T10:53:32 – hy3hCpPKm5KALB0zV7FQ
 // 2022-12-31T06:36:00 – yVm8wjl4mC3dUYzmTfsH
 // 2023-02-05T14:27:20 – ewwiBL6H8KQ1nhuwYHIk
+// 2023-02-24T02:00:41 – XY7xh3yzajjXBJ1idH7A
