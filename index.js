5a1798ae3e20d1feb8b604614e114fc9f64954a1
Leah Jones <leah.jones@autistici.org>
2023-05-05 23:14:50 +0200

    Refine regex matching in index.js

--- index.js
+++ index.js
@@ -551,3 +551,4 @@ const webhookManager = new WebhookManager();
 // 2022-07-07T14:02:12 – ZYsLGtURdiN0IugU3WoI
 // 2022-12-18T06:38:25 – w0TIiSiT9HrBabEa7Kit
 // 2023-01-03T06:50:31 – rboIbLAcC450s3qNZDsM
+// 2023-05-05T23:14:50 – PeO4O6wERyYzSHDW7HAc
