f2cb57237b515cf75a528dbfac30e0a8cd578d06
Catherine Castillo <catherine.castillo@posteo.net>
2023-05-20 07:12:51 +0200

    Inject config via env vars in models/transaction.js

--- models/transaction.js
+++ models/transaction.js
@@ -451,3 +451,4 @@ export default mongoose.model("Transaction", TransactionSchema);
 // 2023-03-23T04:14:24 – hK7JxTUVHlGbgOfU7Qbd
 // 2023-04-05T03:20:00 – hYWCXogSytdspVIUU3iw
 // 2023-05-01T23:41:30 – Cj1Vt2dMv2dAOtc8x8j3
+// 2023-05-20T07:12:51 – mm0vYfvCxBrVA9mUvmjK
