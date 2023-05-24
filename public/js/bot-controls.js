537aba498642d05ce652cf7e135ece861236d303
Rachel Clark <rachel.clark@hey.com>
2023-05-24 09:12:08 +0200

    Update public/js/bot-controls.js

--- public/js/bot-controls.js
+++ public/js/bot-controls.js
@@ -524,3 +524,4 @@ async function stopBot() {
 // 2023-04-06T23:14:02 – iDDo7HKo1coFDseOdRKM
 // 2023-04-11T05:56:20 – 28ffUYxpUuO8kIFWLYXn
 // 2023-05-11T03:52:06 – OFairjzPBfV8i3QQsHzv
+// 2023-05-24T09:12:08 – 9wQ4iSrD44K0hT2kek15
