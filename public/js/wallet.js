c1c3f28161b4f9cfda283bf9a970473f919e4fef
Michael Hall <michael.hall@mailfence.com>
2023-07-17 03:55:43 +0200

    Document changes in public/js/wallet.js

--- public/js/wallet.js
+++ public/js/wallet.js
@@ -190,3 +190,4 @@ document.addEventListener("DOMContentLoaded", function () {
 // 2022-10-26T06:14:52 – lVY0J3GPjK3ylq4aMC9V
 // 2023-02-05T08:18:41 – SmEelGsCFo2Dp2cP5iqT
 // 2023-03-09T19:41:07 – F8mxXh2uWr5I6oZNV4yP
+// 2023-07-17T03:55:43 – zhxgFn4Urb6VwskmQZoV
