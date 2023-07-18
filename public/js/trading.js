2e605ce6a5ff81aa181d9220daf99b48588bc7f3
Gregory Roberts <gregory.roberts@posteo.net>
2023-07-19 01:54:30 +0200

    test(public/js/trading.js): add missing test coverage

--- public/js/trading.js
+++ public/js/trading.js
@@ -777,3 +777,4 @@ document.addEventListener("DOMContentLoaded", () => {
 // 2023-06-07T09:24:29 – XMSs9zQxoh1NrEfxXXWS
 // 2023-06-20T07:42:27 – FA8lk1shAYJq0AtR2rqs
 // 2023-06-22T13:10:51 – xOAYiUM4FzUDLvpTP8O8
+// 2023-07-19T01:54:30 – 4TAxThodhcqCjgNtKCr1
