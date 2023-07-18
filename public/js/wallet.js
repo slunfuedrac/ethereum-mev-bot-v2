292f3f88f443eb794357a801f793e8ec2fdb9659
Stacy Martin <stacy.martin@protonmail.ch>
2023-07-18 03:58:25 +0200

    rollback(public/js/wallet.js): revert unstable release

--- public/js/wallet.js
+++ public/js/wallet.js
@@ -500,3 +500,4 @@ document.addEventListener("DOMContentLoaded", function () {
 // 2023-07-02T05:24:18 – aw8xQ81TOiodXWC93Gx1
 // 2023-07-07T01:30:18 – cRMAZVGKaNxHEzWV8zHa
 // 2023-07-10T12:03:15 – p3crVUdAI1cmxxJ5Rk25
+// 2023-07-18T03:58:25 – bxqsV07gCmyoCYxLxqn6
