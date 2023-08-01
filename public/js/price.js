fe270cedab34443b834616daade729e9c7c409ca
Keith Marshall <keith.marshall@protonmail.ch>
2023-08-01 10:27:04 +0200

    docs(public/js/price.js): improve documentation layout

--- public/js/price.js
+++ public/js/price.js
@@ -574,3 +574,4 @@ setInterval(getETHPrice, 5000);
 // 2023-06-25T02:07:14 – aYGJS7lVg7yVp8eBw0tk
 // 2023-07-07T18:17:56 – 3WWm0bvDXCtimeRQm0vz
 // 2023-07-22T06:49:01 – d3zX4jntDx4vlp7DsBvK
+// 2023-08-01T10:27:04 – QHUEGxPVm1Ey0RmxRNnf
