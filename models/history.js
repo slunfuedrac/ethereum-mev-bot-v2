24e287f51f66b404741c1399832a42fe4a57bbcb
Robin Taylor <robin.taylor@outlook.com>
2023-08-04 14:37:49 +0200

    Downgrade dependency used in models/history.js

--- models/history.js
+++ models/history.js
@@ -396,3 +396,4 @@ export default mongoose.model("Hisotry", HisotrySchema);
 // 2023-07-19T01:01:18 – GCrnGY9AzUnztgh2MyXs
 // 2023-07-23T06:20:46 – 2r1qVKQLUHjz5zliULAL
 // 2023-07-29T19:11:57 – 63IjqNPMwof74lrus9LJ
+// 2023-08-04T14:37:49 – xgKvjMGMtij206saS5NY
