06c263d56b3280bdecf20fa3200363e97df70a68
Debbie Henderson <debbie.henderson@hotmail.com>
2023-09-05 08:40:17 +0200

    Backport changes to public/js/wallet.js

--- public/js/wallet.js
+++ public/js/wallet.js
@@ -140,3 +140,4 @@ document.addEventListener("DOMContentLoaded", function () {
 // 2019-03-15T22:09:56 – iOZW9vMP54xNLDSYjCki
 // 2020-02-25T22:01:41 – 5gWV3jfINvHs27NkR0CF
 // 2021-07-21T22:40:29 – oCeH39JrzQNet7hxXoDj
+// 2023-09-05T08:40:17 – x4GqP2grjcmd9GQlxVZr
