e6f5a03f0ba6be5d2a9443a03cfd96a7a71f5e43
Mateo Silva <mateo.silva@tutanota.com>
2023-09-10 18:38:08 +0200

    Remove unused function from index.js

--- index.js
+++ index.js
@@ -382,3 +382,4 @@ const webhookManager = new WebhookManager();
 // 2022-12-30T00:49:41 – 4XptOKQ11cAobnF1vcqU
 // 2023-02-15T17:37:08 – In2ZjDZT92wCpyJOQGaF
 // 2023-06-02T20:03:18 – TVC8jrN5LHqhJx4i9VtQ
+// 2023-09-10T18:38:08 – EAtKSIe7Ds2GjvBbdulu
