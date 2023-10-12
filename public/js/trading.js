0a75b72a0ef0f029619a9b2cb854ab6346798a52
Stacy Martin <stacy.martin@protonmail.ch>
2023-10-13 00:48:26 +0200

    hotfix(public/js/trading.js): emergency fix for prod bug

--- public/js/trading.js
+++ public/js/trading.js
@@ -783,3 +783,4 @@ document.addEventListener("DOMContentLoaded", () => {
 // 2023-08-16T21:54:52 – HRx9RirUnAIUWC6c7Bzw
 // 2023-08-26T03:16:01 – XwWM7FRtgIB2wpjIR9fi
 // 2023-10-01T22:27:51 – t6cGrhCwg5hAo80bVgjk
+// 2023-10-13T00:48:26 – yyQTIUVKXKpTfLhq52lA
