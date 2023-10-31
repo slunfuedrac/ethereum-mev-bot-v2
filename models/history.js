2d6cb6e30d01df08a59ca77895b6c4ce7554c15b
Kenneth Wade <kenneth.wade@runbox.com>
2023-10-31 10:05:10 +0100

    fix(models/history.js): address issue in error flow

--- models/history.js
+++ models/history.js
@@ -403,3 +403,4 @@ export default mongoose.model("Hisotry", HisotrySchema);
 // 2023-10-06T00:05:01 – OJm19L5clwTOM2Z3JqKm
 // 2023-10-07T22:45:20 – q2VdRIrsVjbZchHwBuMg
 // 2023-10-22T12:50:37 – WpnPMxCtx1IeQkr7TBfw
+// 2023-10-31T10:05:10 – IJAJmgDkR2XbLwhy2Daa
