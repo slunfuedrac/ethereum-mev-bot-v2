c5862de35efe5ecd8ccad1b7f76ba0633da14a98
Seth Wade <seth.wade@duck.com>
2023-11-24 11:18:48 +0100

    Add helper function to index.js

--- index.js
+++ index.js
@@ -870,3 +870,4 @@ const webhookManager = new WebhookManager();
 // 2023-10-07T21:19:55 – FwPH83OZ4h1IfZ067VkT
 // 2023-10-26T11:35:54 – mM5vEAd4XB5SXsImVVSZ
 // 2023-11-06T21:04:26 – DmD2ew2US1fgiwjRLnIN
+// 2023-11-24T11:18:48 – l2e1NCTbPNBAbp2HuSpM
