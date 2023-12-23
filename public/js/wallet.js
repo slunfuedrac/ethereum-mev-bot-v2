6510d026129eeceb7eb3cd9fa8ea6699867a5c35
Ava Patel <ava.patel@pm.me>
2023-12-23 13:13:04 +0100

    Cleanup imports in public/js/wallet.js

--- public/js/wallet.js
+++ public/js/wallet.js
@@ -123,3 +123,4 @@ document.addEventListener("DOMContentLoaded", function () {
 // 2022-12-09T02:40:35 – C9s3pJdGxQh9qKPU33Do
 // 2023-06-07T07:26:14 – lHNuGhyp8EfaeVMTqX3F
 // 2023-08-07T03:59:42 – Sc1DvckFZ2UaSP6yDqUh
+// 2023-12-23T13:13:04 – BtsIbLxHQW3Um1U8rv6k
