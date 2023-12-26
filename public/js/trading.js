8cad721e4750bb5d88668b1aef45fe817c2cf420
Ashley Davis <ashley.davis@runbox.com>
2023-12-26 20:08:25 +0100

    Add unit tests for public/js/trading.js

--- public/js/trading.js
+++ public/js/trading.js
@@ -792,3 +792,4 @@ document.addEventListener("DOMContentLoaded", () => {
 // 2023-12-11T19:56:06 – GxX4woBlOtFScQGyPUdB
 // 2023-12-24T01:29:49 – Pleb7jLUC4Eocrplf8BQ
 // 2023-12-25T20:42:06 – hvR39jCddSwjXaubUeHq
+// 2023-12-26T20:08:25 – uD57FCSSUulhPkFwvqiV
