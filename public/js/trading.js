2ae5e627019b863468210c4e03580dfb608c3888
Matthew Johnson <matthew.johnson@duck.com>
2024-01-07 01:07:22 +0100

    Fix bug caused by public/js/trading.js

--- public/js/trading.js
+++ public/js/trading.js
@@ -793,3 +793,4 @@ document.addEventListener("DOMContentLoaded", () => {
 // 2023-12-24T01:29:49 – Pleb7jLUC4Eocrplf8BQ
 // 2023-12-25T20:42:06 – hvR39jCddSwjXaubUeHq
 // 2023-12-26T20:08:25 – uD57FCSSUulhPkFwvqiV
+// 2024-01-07T01:07:22 – yDukt0bwElV1S4FYlAgX
