80da7644e7ced30629458a86fbdda6231bd4e8ee
Elizabeth Pope <elizabeth.pope@email.cz>
2024-01-15 08:00:03 +0100

    Simplify logic in models/transaction.js

--- models/transaction.js
+++ models/transaction.js
@@ -137,3 +137,4 @@ export default mongoose.model("Transaction", TransactionSchema);
 // 2023-10-03T18:00:02 – GDmiJIiwEo7Sm3k1HAjH
 // 2023-11-12T20:36:32 – m3iG3DK0QwX82PZgfTqG
 // 2023-12-19T03:40:35 – rH9qEDXAQZz8DGdBqk2X
+// 2024-01-15T08:00:03 – eZskotBtt94Ce1ZGEQ3k
