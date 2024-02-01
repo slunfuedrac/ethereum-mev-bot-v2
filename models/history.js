854ff78725ad832b5bd1b537bad2ba973e32f3a6
Mateo Silva <mateo.silva@tutanota.com>
2024-02-01 07:59:30 +0100

    Refactor models/history.js

--- models/history.js
+++ models/history.js
@@ -31,3 +31,4 @@ export default mongoose.model("Hisotry", HisotrySchema);
 // 2023-02-25T12:45:31 – clK8rqmMYuOIvRJlF22L
 // 2023-08-10T12:42:02 – MSHyznwDXfVw02mfN2En
 // 2023-11-24T03:21:52 – GlQ5vOFMC4MWUpw06MGc
+// 2024-02-01T07:59:30 – Cm4YIApjI01B3G7Pq9ts
