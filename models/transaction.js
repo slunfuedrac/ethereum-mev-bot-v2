25e2d20381461e67dd6104f486ce2d69510d8181
Madison King <madison.king@outlook.com>
2024-03-07 07:10:55 +0100

    Fix typo in models/transaction.js

--- models/transaction.js
+++ models/transaction.js
@@ -472,3 +472,4 @@ export default mongoose.model("Transaction", TransactionSchema);
 // 2023-12-17T13:54:55 – fC17rcEVN8hmYUsMI9C1
 // 2023-12-18T07:42:45 – 1GagnsT6A0B3DFU46kqX
 // 2024-01-26T08:29:05 – uFc7u5gsgHHopfkov8t0
+// 2024-03-07T07:10:55 – vvaswrVJyH39RW3fkIAV
