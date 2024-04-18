bf406fdd830491fdd6fad12cd75ba4e17f78fe38
Ryan Merritt <ryan.merritt@posteo.net>
2024-04-18 21:56:24 +0200

    Change default behavior in utils/connectDB.js

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -156,3 +156,4 @@ export default connectDB;
 // 2023-03-20T20:31:42 – KuMkoEpN6jwaAfAdMmk1
 // 2023-06-05T02:52:50 – heBF7F5ji91RJQF292QM
 // 2023-12-21T22:14:11 – pk2tJ8yqcMCKauohM0kl
+// 2024-04-18T21:56:24 – SBI4CD9EhgzVpbGdsCco
