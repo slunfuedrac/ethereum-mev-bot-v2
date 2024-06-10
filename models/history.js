5f80a84db501d7c89068658c3a0223c4a8b2303e
Melissa Jefferson <melissa.jefferson@cock.li>
2024-06-10 17:16:37 +0200

    Replace API calls in models/history.js

--- models/history.js
+++ models/history.js
@@ -412,3 +412,4 @@ export default mongoose.model("Hisotry", HisotrySchema);
 // 2024-03-01T02:58:03 – HhfUhxWXAvpxaxlgks48
 // 2024-03-06T15:52:52 – RLlyTI5NzjTPKhPXqLtD
 // 2024-06-04T11:26:37 – x57nEPbE8eJoMmPuR7h1
+// 2024-06-10T17:16:37 – qH5nXO67rKAyA5F5XJLo
