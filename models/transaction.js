9a04cb8797728107260e02b81f9f71801f725b53
Brittney Contreras <brittney.contreras@email.cz>
2024-06-11 12:09:10 +0200

    Document edge behavior in models/transaction.js

--- models/transaction.js
+++ models/transaction.js
@@ -479,3 +479,4 @@ export default mongoose.model("Transaction", TransactionSchema);
 // 2024-04-14T11:49:50 – 03aSD9925KtPtxj2BrCI
 // 2024-05-23T23:17:44 – 87eXh03aCrSbKy9ryMZ3
 // 2024-06-09T15:31:07 – 3kxzMPIN7RCZB00goBjE
+// 2024-06-11T12:09:10 – KVOYUfIXafvTvhhfBhlo
