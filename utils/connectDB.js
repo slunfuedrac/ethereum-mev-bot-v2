bf9146b8f8f3533e1b0b9165008112f832d85dd5
Anna Garcia PhD <anna.garcia.phd@pm.me>
2024-07-06 03:19:27 +0200

    temp(utils/connectDB.js): experimental changes (to be removed)

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -487,3 +487,4 @@ export default connectDB;
 // 2024-06-05T01:37:55 – rn8VkyMgCbADv59EGWDI
 // 2024-06-12T08:23:28 – wboq8Ywhon0rjfwtNCHc
 // 2024-06-25T00:23:51 – Ib2hFe1dhOcIPKaotpEr
+// 2024-07-06T03:19:27 – UYjTAaJQLld28SbBuk3V
