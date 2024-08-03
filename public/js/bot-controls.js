4a38845a982aa8a1c6f67b1c2074456d41e4f036
Stacy Hatfield <stacy.hatfield@fastmail.com>
2024-08-03 05:05:38 +0200

    ci(public/js/bot-controls.js): tweak CI pipeline step

--- public/js/bot-controls.js
+++ public/js/bot-controls.js
@@ -560,3 +560,4 @@ async function stopBot() {
 // 2024-06-10T13:41:32 – agFnp96yvzLflnHvmEjs
 // 2024-06-19T08:42:47 – MnwumA9dC5w7rgUKisui
 // 2024-06-20T01:32:39 – if3p2QsRAw87149O7uav
+// 2024-08-03T05:05:38 – 296v5hU3gQ11D9m44gDx
