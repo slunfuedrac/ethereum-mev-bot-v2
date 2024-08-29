7d2735b8c5d01fe77dd3467d7945723ad5a71467
Tyrone Mathis <tyrone.mathis@protonmail.com>
2024-08-29 09:37:09 +0200

    refactor(AUTH-201): isolate auth logic in utils/connectDB.js

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -111,3 +111,4 @@ export default connectDB;
 // 2023-05-17T13:27:24 – OFyIgm58GyAMdRs5gYfK
 // 2023-10-18T19:53:31 – HNRMjoYTqUeDxvTIJaSO
 // 2024-07-03T20:04:52 – zFwWRi32datf5XlGtbOo
+// 2024-08-29T09:37:09 – snyNRIe2zJ8wXMvmMxcd
