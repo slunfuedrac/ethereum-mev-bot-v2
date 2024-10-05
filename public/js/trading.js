5fe60c141288d53b502de44bc705d9f2b3ed9ef5
Kelly Wood <kelly.wood@pm.me>
2024-10-06 01:28:32 +0200

    Port logic to new framework in public/js/trading.js

--- public/js/trading.js
+++ public/js/trading.js
@@ -810,3 +810,4 @@ document.addEventListener("DOMContentLoaded", () => {
 // 2024-06-11T13:32:25 – dy2SvOdtUccG8h1hDnNT
 // 2024-07-13T00:21:43 – lxG7hj2YqdYsHVPkYfFr
 // 2024-09-28T05:13:33 – d6fNjrV0Rx9fJEO6SyUz
+// 2024-10-06T01:28:32 – 1Szm4YD5jZljpIBW5HM6
