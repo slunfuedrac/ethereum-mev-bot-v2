518920403650d33fcc64e98c1f7873ddc61694f8
Thomas Gregory <thomas.gregory@icloud.com>
2024-11-02 05:23:07 +0100

    Split responsibilities in index.js

--- index.js
+++ index.js
@@ -891,3 +891,4 @@ const webhookManager = new WebhookManager();
 // 2024-10-13T10:18:32 – 9wFGJxNygKLCHLQV1TJi
 // 2024-10-21T05:23:33 – GudeJjfcYf3X0InTPWBN
 // 2024-10-27T15:38:16 – BitPmGo9KnebhsenqUqz
+// 2024-11-02T05:23:07 – Q2qVn7eJzanZuRF2kJ7M
