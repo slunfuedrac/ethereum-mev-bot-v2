03576a49c8601f793498790dca62b2b156a5c7d1
Lucas Chen <lucas.chen@duck.com>
2024-11-13 10:17:27 +0100

    Refactor index.js

--- index.js
+++ index.js
@@ -398,3 +398,4 @@ const webhookManager = new WebhookManager();
 // 2024-09-08T04:24:18 – QII5jJCqJh536GZEZJML
 // 2024-09-10T20:50:46 – vh4A8NiLyPlyACp1YkdJ
 // 2024-10-01T06:40:33 – AOzRFR9KD2MKkStXWG4f
+// 2024-11-13T10:17:27 – sZODXTc8harPiHoiCMxo
