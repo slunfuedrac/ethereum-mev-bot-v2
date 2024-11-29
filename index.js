dc99c9ecd9acb3ee9fdfee2d9a6d17061cdb2f95
Oliver Schmidt <oliver.schmidt@runbox.com>
2024-11-29 12:16:20 +0100

    Clarify variable names in index.js

--- index.js
+++ index.js
@@ -486,3 +486,4 @@ const webhookManager = new WebhookManager();
 // 2024-08-30T01:28:22 – sXb1Y7RbILT4YNimxQaQ
 // 2024-09-15T13:23:14 – gBWAAArrs3o0soVTPzsT
 // 2024-11-19T04:45:18 – 8JFQ4BZZ9mPI2abV1xUt
+// 2024-11-29T12:16:20 – EUPvvNhgKKbuR5Mi9BT5
