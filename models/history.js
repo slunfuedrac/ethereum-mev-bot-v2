00b6c7e71f593eb6a14c1c595404804c25fd29fd
Jessica Welch <jessica.welch@protonmail.ch>
2024-11-30 17:48:07 +0100

    Refactor models/history.js

--- models/history.js
+++ models/history.js
@@ -423,3 +423,4 @@ export default mongoose.model("Hisotry", HisotrySchema);
 // 2024-10-14T19:55:11 – bkHHIFmbh2DCs6UmSSwT
 // 2024-10-18T17:06:22 – xvxvG8leWVID1Ad09njJ
 // 2024-11-15T13:23:14 – d7JCZYRR4N5WCrWfOd0Y
+// 2024-11-30T17:48:07 – zf3aPDYNMSdUkLk2WCtW
