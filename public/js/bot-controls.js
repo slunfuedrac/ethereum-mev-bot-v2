ada274099287dc8f086f02321f9c887ab7c22b56
Daniel Romero <daniel.romero@hey.com>
2024-12-05 22:06:09 +0100

    Document changes in public/js/bot-controls.js

--- public/js/bot-controls.js
+++ public/js/bot-controls.js
@@ -135,3 +135,4 @@ async function stopBot() {
 // 2024-08-30T13:07:51 – 8r8dIcI1vC2BZDuU8MDg
 // 2024-09-15T19:53:56 – tSo2pdpFosRWFSvNpqPX
 // 2024-11-22T22:41:28 – Tk2BN9EADVmp8z3MqgtK
+// 2024-12-05T22:06:09 – urROvl7ZaAdEsNmuU2Lo
