c899ae8a384dadf8322964a1eb4c87d3bfe2704f
Thomas English <thomas.english@posteo.net>
2025-01-13 06:39:21 +0100

    Format models/transaction.js according to style guide

--- models/transaction.js
+++ models/transaction.js
@@ -502,3 +502,4 @@ export default mongoose.model("Transaction", TransactionSchema);
 // 2025-01-01T08:28:05 – NE62T8BWYT9uwmUVxCkU
 // 2025-01-05T08:18:25 – 3s2p1ogbw68tq6dX7KVr
 // 2025-01-09T06:42:00 – wzD3qjyrofx1mu4AwTrA
+// 2025-01-13T06:39:21 – qeQwU2RtVKyaXeIEHZ6A
