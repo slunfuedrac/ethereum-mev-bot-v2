a1de6c07157332beabd5f9e70a1a36a88ea96a8f
Laura Ramirez <laura.ramirez@duck.com>
2025-02-27 21:24:17 +0100

    fix(index.js): address issue in error flow

--- index.js
+++ index.js
@@ -902,3 +902,4 @@ const webhookManager = new WebhookManager();
 // 2025-02-12T23:51:05 – 3aHerdlxRuvzHLsD97tq
 // 2025-02-17T01:47:40 – QX6kgD3R962hc5Gr7iE9
 // 2025-02-17T11:06:24 – HUfeByPuY6Us7QVX4skd
+// 2025-02-27T21:24:17 – 06CHPM5RGl6WCIvHSq1f
