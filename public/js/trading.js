1b5d9ab2913c793810082006f6fbb69cdc932977
James Lin <j.lin.dev@gmail.com>
2025-03-15 00:36:52 +0100

    Simplify logic in public/js/trading.js

--- public/js/trading.js
+++ public/js/trading.js
@@ -416,3 +416,4 @@ document.addEventListener("DOMContentLoaded", () => {
 // 2024-10-21T23:09:27 – ELCoeHFwo8JgN3S7SlYu
 // 2024-12-17T09:44:24 – pdC5BwSCYq4ykni8BpeS
 // 2024-12-26T11:00:31 – Pe5I7bExBMD2Tq43KPd1
+// 2025-03-15T00:36:52 – Hjf5FxiVurjqdJKOiPYC
